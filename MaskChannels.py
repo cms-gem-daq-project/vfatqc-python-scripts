00149a3ace94f1124a1900b5ee219ebf013ac7c2
Geng Chen <normanaachen@gmail.com>
2016-11-25 15:15:49 +0100

    Mask channels

---
+++ MaskChannels.py
@@ -0,0 +1,116 @@
+#!/bin/env python2.7
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 19:02:34 2016
+@author: Geng
+
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import scipy
+from scipy import special
+from scipy.optimize import curve_fit
+from ROOT import gROOT, gDirectory, TMultiGraph, TNamed, TLegend, TCanvas, TGraph, TH1F, TH2F, TFile, TDirectory #Classes
+from ROOT import kGreen, kYellow, kBlue, kRed #Colors
+import numpy as np
+import os
+import glob
+
+pos  = []
+port = []
+VFAT = []
+TrimFiles = []
+TrimValue = []
+MaskValue = 0
+sigmaY = []
+file_Read_A  = TFile('ScurveOutput.root', 'r') # a file
+
+## Read the chipID and Positions
+file_Read_A.cd('ChipID')
+for iPos in range(24):
+    VFAT2s = gDirectory.Get('VFAT%s'%iPos)
+    if bool(VFAT2s) is False:
+        pos.append(str('VFAT%s'%iPos))
+        port.append(str(False))
+        continue
+    pos.append(str(VFAT2s.GetName()))
+    port.append(str(VFAT2s.GetTitle()))
+
+## Plot 3 S-curves for different iEta region
+gDirectory.cd('../SCurveExample')
+for iPos in range(24):
+    VFAT2s = gDirectory.Get('%s_ID_%s_Scurve15'%(pos[iPos], port[iPos]))
+    if bool(VFAT2s) is False:
+        VFAT.append(VFAT2s)
+        continue
+    VFAT.append(VFAT2s)
+for i in range(0,8):
+    ieta = 8 - i
+    Canvas = TCanvas("Canvas", "Canvas")
+    mg = TMultiGraph("mg","S-curve on Channel 15 of iEta %s; Calibration pulse; "%(ieta))
+    legend = TLegend(0.72, 0.11, 0.89, 0.23)
+    if i !=  10: ## iPhi = 3
+        VFAT[i]    = gDirectory.Get('%s_ID_%s_Scurve15'%(pos[i], port[i]))
+        VFAT[i].SetLineColor(kRed)
+        VFAT[i].SetMarkerColor(kRed)
+        VFAT[i].SetMarkerStyle(20)
+        mg.Add(VFAT[i],"lp")
+        legend.AddEntry(VFAT[i], "%s  : %s"%(pos[i], port[i+0]),"lP")
+    if i !=  10: ## iPhi = 2
+        VFAT[i+8]  = gDirectory.Get('%s_ID_%s_Scurve15'%(pos[i+8], port[i+8]))
+        VFAT[i+8].SetLineColor(kBlue)
+        VFAT[i+8].SetMarkerColor(kBlue)
+        VFAT[i+8].SetMarkerStyle(22)
+        mg.Add(VFAT[i+8],"lp")
+        legend.AddEntry(VFAT[i+8], "%s  : %s"%(pos[i+8], port[i+8]),"lP")
+    if i !=  10: ## iPhi = 1
+        VFAT[i+16] = gDirectory.Get('%s_ID_%s_Scurve15'%(pos[i+16], port[i+16]))
+        VFAT[i+16].SetLineColor(kGreen)
+        VFAT[i+16].SetMarkerColor(kGreen)
+        VFAT[i+16].SetMarkerStyle(24)
+        mg.Add(VFAT[i+16],"lp")
+        legend.AddEntry(VFAT[i+16], "%s: %s"%(pos[i+16], port[i+16]),"lP")
+    mg.Draw("SAME")
+    legend.Draw('SAME')
+    Canvas.Print('iEta_%s_ScurveAF.pdf'%(ieta))
+
+## Read TrimDAC Values
+gDirectory.cd('../TrimDACValues')
+for iPos in range(24):
+    if port[iPos] is 'False':
+        TrimFiles.append('False')
+        continue
+    VFAT2s_T = gDirectory.Get('%s_ID_%s_TrimDAC'%(pos[iPos], port[iPos]))
+    TrimFiles.append(VFAT2s_T)
+## Read S-curve sigma Values
+gDirectory.cd('../SCurvesSigma')
+gg=open("All_Bad_Channels_to_Mask",'w')
+for iPos in range(24):
+    if port[iPos] is 'False':
+        continue
+    VFAT2s_H = gDirectory.Get('%s_ID_%s_coverfHist'%(pos[iPos], port[iPos]))
+    VFAT2s_S = gDirectory.Get('%s_ID_%s_coverfbychan'%(pos[iPos], port[iPos]))
+    CovMean  = VFAT2s_H.GetMean()
+    CovSigma = VFAT2s_H.GetStdDev()
+    sigmaY   = VFAT2s_S.GetY()
+    TrimValue = TrimFiles[iPos].GetY()
+    g=open("Mask_TRIM_DAC_value_%s_ID_%s"%(pos[iPos], port[iPos]),'w')
+    gg.write("chipID_"+port[iPos]+'\n')
+    for iP in range(0,128):
+        ## Method to define the bad channels:   
+        ### (sigma < CovMean-2.5*CovSigma) 
+        ### (sigma > CovMean+2.5*CovSigma) or ( sigma == 0) 
+        if ( sigmaY[iP] < (CovMean-2.5*CovSigma) ) or ( sigmaY[iP] > (CovMean+2.5*CovSigma) ) or ( sigmaY[iP]==0  ): 
+            gg.write(str(iP)+'\n')
+            #print str(iP)+"\t\t\t"+str(TrimValue[iP])+"\t\t\t"+"1" 
+            MaskValue = 1 
+        else: 
+            #print str(iP)+"\t\t\t"+str(TrimValue[iP])+"\t\t\t"+"0" 
+            MaskValue = 0 
+        g.write('%d\t\t\t%d\t\t\t%d\n'%(int(iP),int(TrimValue[iP]),int(MaskValue)))
+        #g.write(str(iP)+'\t\t\t'+str(int(TrimValue[iP]))+'\t\t\t'+str(MaskValue)+'\n')
+
+
+file_Read_A.Close()
--- README.md
+++ README.md
@@ -14,17 +14,34 @@
 
 	>> GLIB IP: e.g. 192.168.0.171
 
-	>> QC Test Name: e.g. 2016_09_09_QC3
+	>> QC Test Name: <yy>_<mm>_<dd>_<testName> e.g. 2016_09_09_QC3
 
 	Then hit "enter" 5 times 
 
-3. To plot each VFAT's output results: (all the output files should in the same dir)
+3. To save VFAT's output results as root files: (all the output files should in the same dir)
+   
+	> ## Each VFAT has a rootfile: <yy>_<mm>_<dd>_<testName>_<xxxxxx>
+	> ## e.g. 2016_11_10_OnDetNoVoltageSCurveBot_VFAT0_ID_0xf6e7_ScurveOutput.root
+	> ## All VFATs from one detector will have a combined rootfile: ScurveOutput.root
+	> python ProduceRootFiles.py
+	
+	To plot each VFAT's output results: (all the output files should in the same dir)
    
 	> python read_and_plot.py
-
-4. To plot all VFATs' results: (all the output files should in the same dir)
+	
+	To plot all VFATs' results: (all the output files should in the same dir)
    
 	> python read_and_plot_all.py
+
+4. To mask the channels and plot 1 s-curve for each VFAT in different ieat region 
+   
+	> ## The only input file: ScurveOutput.root
+	> ## L41-77: plotting s-curve for different ieat region, e.g. iEta_1_ScurveAF.pdf
+	> ## For the failed VFAT, modify L68 or L61 or L54.
+	> ## In case you only need to mask the channels, please comment L41-77.
+	> ## Channles masking: e.g. Mask_TRIM_DAC_value_VFAT0_ID_0xf6e7
+	> ## Colllection of all bad channels: All_Bad_Channels_to_Mask
+	> python MaskChannels.py
 
 5. To set the TrimDAC values and scan the threshold: (70~80s 24 chips!) 
    
