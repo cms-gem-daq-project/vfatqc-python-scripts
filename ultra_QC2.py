62d46fa63ef7d9576d671ebe3516685efc27f41e
Reyer Band <rband@ucdavis.edu>
2016-12-15 19:35:31 +0100

    Updates and Cleaning

--- ultra_QC2.py
+++ ultra_QC2.py
@@ -62,7 +62,7 @@ testSuite.runSelectedTests()
 testSuite.report()
 
     ########################## The Script ################################
-THRESH_ABS = 0.1
+THRESH_ABS = 0.0
 THRESH_REL = 0.05
 THRESH_MAX = 254
 THRESH_MIN = 0
@@ -72,7 +72,7 @@ N_EVENTS = 1000.00
 N_EVENTS_SCURVE = 1000.00
 CHAN_MIN = 0
 CHAN_MAX = 128
-
+MAX_TRIM_IT = 20
 TotVCal = {}
 VCal_ref  = {}
 
@@ -112,6 +112,7 @@ for port in testSuite.presentVFAT2sSingle:
     pass
     ################## Threshold Scan For All VFAT2 #########################
 biasAllVFATs(testSuite.glib, options.gtx, 0, enable = True, debug = options.debug) #Not sure this should be set
+writeAllVFATs(testSuite.glib, options.gtx, "Latency", 37)
 configureScanModule(testSuite.glib, options.gtx, 0, 0, scanmin = THRESH_MIN, scanmax = THRESH_MAX, numtrigs = int(N_EVENTS), useUltra = True, debug = options.debug)
 printScanConfiguration(testSuite.glib, options.gtx, useUltra = True, debug = options.debug)
 startScanModule(testSuite.glib, options.gtx, useUltra = True, debug = options.debug)
@@ -133,7 +134,7 @@ for n in testSuite.presentVFAT2sSingle:
         lastnoise = 100*(data_threshold[d-1] & 0xffffff)/(1.*N_EVENTS)
         nextnoise = 100*(data_threshold[d+1] & 0xffffff)/(1.*N_EVENTS)
         
-        passAbs     = (noise) < THRESH_ABS
+        passAbs     = (noise) <= THRESH_ABS
         passLastRel = (lastnoise - noise) < THRESH_REL
         passNextRel = abs(noise - nextnoise) < THRESH_REL
         
@@ -170,6 +171,8 @@ configureLocalT1(testSuite.glib, options.gtx, 1, 0, 40, 200, 0, options.debug)
 startLocalT1(testSuite.glib, options.gtx)
     #### With TRIM DAC to 0
 for channel in range(CHAN_MIN, CHAN_MAX):
+    if ((options.debug) and (channel == 1)):
+        break
     for trim in [0,16,31]:
         writeAllVFATs(testSuite.glib, options.gtx, "VFATChannels.ChanReg%d"%(channel+1), 64+trim)
         configureScanModule(testSuite.glib, options.gtx, 3, 0, channel = channel, scanmin = SCURVE_MIN, scanmax = SCURVE_MAX, numtrigs = int(N_EVENTS_SCURVE), useUltra = True, debug = options.debug)
@@ -222,55 +225,69 @@ for channel in range(CHAN_MIN, CHAN_MAX):
                 pass
             m.close()
             pass
-        writeAllVFATs(testSuite.glib, options.gtx, "VFATChannels.ChanReg%d"%(channel+1), trim)
+        writeAllVFATs(testSuite.glib, options.gtx, "VFATChannels.ChanReg%d"%(channel+1), 0)
         pass
     pass
 print "------Second Debug ----" + str(channel)
     ################## Adjust the trim for each channel ######################
     #    if options.doQC3:
     #        continue
-sys.exit()
 print
 print "------------------------ TrimDAC routine ------------------------"
 print
 for channel in range(CHAN_MIN, CHAN_MAX):
-    if debug:
-        for trim in [0,16,31]:
-            print "TotVCal[%d](length = %d) = %s"%(trim,
-                                                   len(TotVCal["%d"%(trim)]),
-                                                       TotVCal["%d"%(trim)])
-            pass
-        pass
+#    if options.debug:
+#        for trim in [0,16,31]:
+#            print "TotVCal[%d](length = %d) = %s"%(trim,
+#                                                   len(TotVCal["%d"%(trim)]),
+#                                                       TotVCal["%d"%(trim)])
+#            pass
+#        pass
+    if ((options.debug) and (channel == 1)):
+        break
     for n in testSuite.presentVFAT2sSingle:
+        if ((options.debug) and (n == 1)):
+            break
         h=open("%s_VCal_VFAT2_%d_ID_0x%04x"%(str(Date),n,testSuite.chipIDs[n]&0xffff),'a')
         try:
             VCal_ref[str(n)+"0"] = sum(TotVCal[str(n)+"0"])/len(TotVCal[str(n)+"0"])
-            h.write(str(TotVCal0[str(n)+"0"])+"\n")
+            h.write(str(TotVCal[str(n)+"0"])+"\n")
             VCal_ref[str(n)+"31"] = sum(TotVCal[str(n)+"31"])/len(TotVCal[str(n)+"31"])
             h.write(str(TotVCal[str(n)+"31"])+"\n")
             VCal_ref[str(n)+"avg"] = (VCal_ref[str(n)+"0"] + VCal_ref[str(n)+"31"])/2
             print "VCal_ref0", VCal_ref[str(n)+"0"]
             print "VCal_ref31", VCal_ref[str(n)+"31"]
+            print VCal_ref
         except:
             ex = sys.exc_info()[0]
             print "Caught exception: %s"%(ex)
             print "S-Curve did not work"
-            h.close()
+            print VCal_ref
+            pass
+        h.close()
         pass
-        TRIM_IT = [0] * 23
-        print "TrimDAC Channel%d" %channel
-        trimDAC = [16] * 23
-        foundGood = False
-        
-        while (foundGood == False):
-
-
-            while (glib.get("ultra_status") != 0): i = 1
-            for n in testSuite.presentVFAT2sSingle:
-                regValue = (1 << 6) + trimDAC[n]
+    TRIM_IT = [0] * 24
+    print "TrimDAC Channel%d" %channel
+    trimDAC = [16] * 24
+    foundGood = [False] * 24
+    print foundGood
+    total_goodness = False
+    foundVCal = 0
+    while (total_goodness == False):
+        configureScanModule(testSuite.glib, options.gtx, 3, 0, channel = channel, scanmin = SCURVE_MIN, scanmax = SCURVE_MAX, numtrigs = int(N_EVENTS_SCURVE), useUltra = True, debug = options.debug)
+        printScanConfiguration(testSuite.glib, options.gtx, useUltra = True, debug = options.debug)
+        startScanModule(testSuite.glib, options.gtx, useUltra = True, debug = options.debug)
+        SCurve_Ultra_Results = getUltraScanResults(testSuite.glib, options.gtx, SCURVE_MAX - SCURVE_MIN + 1, options.debug)
+        bool_it = True
+        for n in testSuite.presentVFAT2sSingle:
+            print "On Chip %d"%n
+            if not (foundGood[n]):
+                writeVFAT(testSuite.glib, options.gtx, n, "VFATChannels.ChanReg%d"%(channel+1), 64+trimDAC[n])
                 f = open("%s_Data_GLIB_IP_%s_VFAT2_%d_ID_0x%04x"%(str(Date),str(options.slot),n,testSuite.chipIDs[n]&0xffff),'a')
                 g = open("%s_TRIM_DAC_value_VFAT_%d_ID_0x%04x"%(str(Date),n,testSuite.chipIDs[n]&0xffff),'a')
-                data_trim = glib.fifoRead('ultra_data'+str(n), VCAL_MAX - VCAL_MIN)
+                data_trim = SCurve_Ultra_Results[n]
+                h = open("%s_VCal_VFAT2_%d_ID_0x%04x"%(str(Date),port,testSuite.chipIDs[n]&0xffff),'a')
+
                 try:
                     for d in data_trim:
                         Eff = (d & 0xffffff) / N_EVENTS_SCURVE
@@ -284,71 +301,75 @@ for channel in range(CHAN_MIN, CHAN_MAX):
                 except:
                     print "Error while reading the data, they will be ignored"
                     continue
-                
-                if (foundVCal > VCal_ref[n] and TRIM_IT[n] < MAX_TRIM_IT and trimDAC[n] < 31):
+                if (foundVCal > VCal_ref[str(n) + "avg"] and TRIM_IT[n] < MAX_TRIM_IT and trimDAC[n] < 31):
                     trimDAC[n] += 1
                     TRIM_IT[n] +=1
-                elif (foundVCal < VCal_ref[n] and TRIM_IT[n] < MAX_TRIM_IT and trimDAC[n] > 0):
+                elif (foundVCal < VCal_ref[str(n) + "avg"] and TRIM_IT[n] < MAX_TRIM_IT and trimDAC[n] > 0):
                     trimDAC[n] -= 1
                     TRIM_IT[n] +=1
                 else:
                     g.write(str(trimDAC[n])+"\n")
                     TotFoundVCal.append(foundVCal)
                     f.write("S_CURVE_"+str(channel)+"\n")
+                    foundGood[n] = True
                     for d in data_trim:
                         f.write(str((d & 0xff000000) >> 24)+"\n")
-                        f.write(str((d & 0xffffff)/N_EVENTS_TRIM)+"\n")
+                        f.write(str((d & 0xffffff)/N_EVENTS_SCURVE)+"\n")
                         pass
-                    break
+                    #break
+                    pass
                 pass
+            print TotFoundVCal
+            h.write(str(TotFoundVCal)+"\n")
+            h.close()
             g.close()
             f.close()
+            bool_it = bool_it & foundGood[n]
             pass
-        glib.set("ei2c_reset", 0)
-        glib.set(regName, 0) # disable cal pulse to channel                                                                                                                                             
-        m.close()
-        h.write(str(TotFoundVCal)+"\n")
-        h.close()
+        total_goodness = bool_it
         pass
-    VCalList = []
-    minVcal = 0
+    #    VCalList = []
+    #    minVcal = 0
+    pass
+pass
+stopLocalT1(testSuite.glib, options.gtx)
     ################# Set all the Trim_DAC to the right value #################
-    for port in presentVFAT2Single:
-        g=open("%s_TRIM_DAC_value_VFAT_%d_ID_0x%04x"%(str(Date),port,testSuite.chipIDs[port]&0xffff),'r')
+for port in presentVFAT2Single:
+    g=open("%s_TRIM_DAC_value_VFAT_%d_ID_0x%04x"%(str(Date),port,testSuite.chipIDs[port]&0xffff),'r')
         #g=open(str(Date)+"_TRIM_DAC_value_VFAT_"+str(port)+"_ID_"+ str(testSuite.chipIDs[port]&0xff),'r')
-        for channel in range(CHAN_MIN, CHAN_MAX):
-            if options.debug:
-                if channel > 10:
-                    continue
-                pass
-            regName = "vfat2_" + str(port) + "_channel" + str(channel + 1)
-            trimDAC = (g.readline()).rstrip('\n')
-            print trimDAC
-            regValue = int(trimDAC)
-            glib.set(regName, regValue)
+    for channel in range(CHAN_MIN, CHAN_MAX):
+        if options.debug:
+            if channel > 10:
+                continue
             pass
-        g.close()
+        regName = "vfat2_" + str(port) + "_channel" + str(channel + 1)
+        trimDAC = (g.readline()).rstrip('\n')
+        print trimDAC
+        regValue = int(trimDAC)
+        glib.set(regName, regValue)
         pass
+    g.close()
+    pass
+sys.exit()
     ########################## Final threshold by VFAT2 ######################
     #        f.write("second_threshold\n")
-    glib.set('ultra_reset', 1)
-    glib.set('ultra_mode', 0)
-    glib.set('ultra_min', 0)
-    glib.set('ultra_max', 255)
-    glib.set('ultra_step', 1)
-    glib.set('ultra_n', N_EVENTS)
-    glib.set('ultra_toggle', 1)
-    while (glib.get("ultra_status") != 0): r = 1
-    for n in range(0, 24):
-        f = open("%s_Data_GLIB_IP_%s_VFAT2_%d_ID_0x%04x"%(str(Date),str(options.slot),n,testSuite.chipIDs[n]&0xffff),'a')
-        f.write("second_threshold\n")
-        data = glib.fifoRead('ultra_data'+str(n), 255)
-        for d in data:
-            f.write(str((d & 0xff000000) >> 24)+"\n")
-            f.write(str(100*(d & 0xffffff)/N_EVENTS)+"\n")
-            pass
-        f.close()
+glib.set('ultra_reset', 1)
+glib.set('ultra_mode', 0)
+glib.set('ultra_min', 0)
+glib.set('ultra_max', 255)
+glib.set('ultra_step', 1)
+glib.set('ultra_n', N_EVENTS)
+glib.set('ultra_toggle', 1)
+while (glib.get("ultra_status") != 0): r = 1
+for n in range(0, 24):
+    f = open("%s_Data_GLIB_IP_%s_VFAT2_%d_ID_0x%04x"%(str(Date),str(options.slot),n,testSuite.chipIDs[n]&0xffff),'a')
+    f.write("second_threshold\n")
+    data = glib.fifoRead('ultra_data'+str(n), 255)
+    for d in data:
+        f.write(str((d & 0xff000000) >> 24)+"\n")
+        f.write(str(100*(d & 0xffffff)/N_EVENTS)+"\n")
         pass
+    f.close()
     pass
 pr.disable()
 s = StringIO.StringIO()
