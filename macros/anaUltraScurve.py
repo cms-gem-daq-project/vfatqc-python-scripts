21d8c7f5686a91d1c821edfa25a52cb371f93101
Reyer Band <rband@ucdavis.edu>
2017-02-20 17:43:56 +0100

    Modified anaUltraScurve.py to put out a TTree with the SCurve fit values

--- macros/anaUltraScurve.py
+++ macros/anaUltraScurve.py
@@ -1,35 +1,61 @@
 from optparse import OptionParser
+from array import array
+from fitScanData import *
 from ROOT import TFile,TTree,TH2D,TCanvas,TPad,gROOT,gStyle
 
 parser = OptionParser()
 
 parser.add_option("-f", "--filename", type="string", dest="filename", default="SCurveData.root",
-                  help="Specify Output Filename", metavar="filename")
+                  help="Specify Input Filename", metavar="filename")
+parser.add_option("-o", "--outfilename", type="string", dest="outfilename", default="SCurveFitData.root",
+                  help="Specify Output Filename", metavar="outfilename")
+
 
 (options, args) = parser.parse_args()
 filename = options.filename
-
+print filename
+outfilename = options.outfilename
 gROOT.SetBatch(True)
 gStyle.SetOptStat(0)
 
 inF = TFile(filename)
 
-vSum = {}
-for i in range(0,24):
-    vSum[i] = TH2D('vSum%i'%i,'vSum%i;Channel;VCal [DAC units]'%i,128,-0.5,127.5,256,-0.5,255.5)
+outF = TFile(outfilename, 'recreate')
 
-for event in inF.scurveTree :
-    vSum[event.vfatN].Fill(event.vfatCH,event.vcal,event.Nhits)
+myT = TTree('scurveFitTree','Tree Holding FitData')
 
-outF = TFile('SCurvePlots.root','recreate')
+vcal = array( 'i', [ 0 ] )
+myT.Branch( 'vcal', vcal, 'vcal/I' )
+vfatN = array( 'i', [ 0 ] )
+myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
+vfatCH = array( 'i', [ 0 ] )
+myT.Branch( 'vfatCH', vfatCH, 'vfatCH/I' )
+trimRange = array( 'i', [ 0 ] )
+myT.Branch( 'trimRange', trimRange, 'trimRange/I' )
+vthr = array( 'i', [ 0 ] )
+myT.Branch( 'vthr', vthr, 'vthr/I' )
+trimDAC = array( 'i', [ 0 ] )
+myT.Branch( 'trimDAC', trimDAC, 'trimDAC/I' )
+threshold = array( 'f', [ 0 ] )
+myT.Branch( 'threshold', threshold, 'threshold/F')
+noise = array( 'f', [ 0 ] )
+myT.Branch( 'noise', noise, 'noise/F')
+
+scanFits = fitScanData(filename)
+
+
+for event in inF.scurveTree:
+    vcal[0] = event.vcal
+    vfatN[0] = event.vfatN
+    vfatCH[0] = event.vfatCH
+    trimRange[0] = event.trimRange
+    vthr[0] = event.vthr
+    trimDAC[0] = event.trimDAC
+    threshold[0] = scanFits[0][event.vfatN][event.vfatCH]
+    noise[0] = scanFits[1][event.vfatN][event.vfatCH]
+    myT.Fill()
+    pass
+
 outF.cd()
-
-canv = TCanvas('canv','canv',500*8,500*3)
-canv.Divide(8,3)
-for i in range(0,24):
-    canv.cd(i+1)
-    vSum[i].Draw('colz')
-    canv.Update()
-    vSum[i].Write()
-canv.SaveAs('SCurveSummary.png')
+outF.Write()
 outF.Close()
