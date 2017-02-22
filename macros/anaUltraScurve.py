3315d622032e8231d138ad04151ac94813292ecd
Reyer Band <rband@ucdavis.edu>
2017-02-22 14:40:32 +0100

    added Chi2

--- macros/anaUltraScurve.py
+++ macros/anaUltraScurve.py
@@ -24,8 +24,6 @@ outF = TFile(outfilename, 'recreate')
 
 myT = TTree('scurveFitTree','Tree Holding FitData')
 
-vcal = array( 'i', [ 0 ] )
-myT.Branch( 'vcal', vcal, 'vcal/I' )
 vfatN = array( 'i', [ 0 ] )
 myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
 vfatCH = array( 'i', [ 0 ] )
@@ -45,19 +43,19 @@ myT.Branch( 'chi2', chi2, 'chi2/F')
 
 scanFits = fitScanData(filename)
 
-
 for event in inF.scurveTree:
-    vcal[0] = event.vcal
-    vfatN[0] = event.vfatN
-    vfatCH[0] = event.vfatCH
-    trimRange[0] = event.trimRange
-    vthr[0] = event.vthr
-    trimDAC[0] = event.trimDAC
-    threshold[0] = scanFits[0][event.vfatN][event.vfatCH]
-    noise[0] = scanFits[1][event.vfatN][event.vfatCH]
-    chi2[0] = scanFits[2][event.vfatN][event.vfatCH]
-    myT.Fill()
-    pass
+    if event.vcal == 1:
+        vfatN[0] = event.vfatN
+        vfatCH[0] = event.vfatCH
+        trimRange[0] = event.trimRange
+        vthr[0] = event.vthr
+        trimDAC[0] = event.trimDAC
+        threshold[0] = scanFits[0][event.vfatN][event.vfatCH]
+        noise[0] = scanFits[1][event.vfatN][event.vfatCH]
+        chi2[0] = scanFits[2][event.vfatN][event.vfatCH]
+        myT.Fill()
+        pass
+    pass 
 
 outF.cd()
 outF.Write()
