0a7d0e29843f10f782da0a0b2f1329c38413ed79
Reyer Band <rband@ucdavis.edu>
2017-03-06 17:54:21 +0100

    Make S-Curve for saving in Trim file

--- macros/anaUltraScurve.py
+++ macros/anaUltraScurve.py
@@ -1,6 +1,7 @@
 from optparse import OptionParser
 from array import array
 from fitScanData import *
+from channelMaps import *
 from ROOT import TFile,TTree,TH2D,TGraph,TGraph2D,TCanvas,TPad,gROOT,gStyle,gPad,TPaveStats
 
 parser = OptionParser()
@@ -11,6 +12,8 @@ parser.add_option("-o", "--outfilename", type="string", dest="outfilename", defa
                   help="Specify Output Filename", metavar="outfilename")
 parser.add_option("-b", "--drawbad", action="store_true", dest="drawbad",
                   help="Draw fit overlays for Chi2 > 10000", metavar="drawbad")
+parser.add_option("-t", "--type", type="string", dest="GEBtype", default="long",
+                  help="Specify GEB (long/short)", metavar="GEBtype")
 
 
 (options, args) = parser.parse_args()
@@ -19,17 +22,46 @@ print filename
 outfilename = options.outfilename
 gROOT.SetBatch(True)
 #gStyle.SetOptStat(0)
-
+GEBtype = options.GEBtype
 inF = TFile(filename)
 
 outF = TFile(outfilename, 'recreate')
 
 myT = TTree('scurveFitTree','Tree Holding FitData')
 
+#Build the channel to strip mapping from the text file
+lookup_table = []
+for vfat in range(0,24):
+    lookup_table.append([])
+    for channel in range(0,128):
+        lookup_table[vfat].append(0)
+        pass
+    pass
+
+if GEBtype is 'long':
+    intext = open('longChannelMap.txt', 'r')
+    for i, line in enumerate(intext):
+        if i == 0: continue
+        mapping = line.rsplit('\t')
+        lookup_table[int(mapping[0])][int(mapping[2]) -1] = int(mapping[1])
+        pass
+    pass
+if GEBtype is 'short':
+    intext = open('shortChannelMap.txt', 'r')
+    for i, line in enumerate(intext):
+        if i == 0: continue
+        mapping = line.rsplit('\t')
+        lookup_table[mapping[0]][mapping[2]] = mapping[1]
+        pass
+    pass
+
+
 vfatN = array( 'i', [ 0 ] )
 myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
 vfatCH = array( 'i', [ 0 ] )
 myT.Branch( 'vfatCH', vfatCH, 'vfatCH/I' )
+vfatstrip = array( 'i', [ 0 ] )
+myT.Branch( 'vfatstrip', vfatstrip, 'vfatstrip/I' )
 trimRange = array( 'i', [ 0 ] )
 myT.Branch( 'trimRange', trimRange, 'trimRange/I' )
 vthr = array( 'i', [ 0 ] )
@@ -42,20 +74,31 @@ noise = array( 'f', [ 0 ] )
 myT.Branch( 'noise', noise, 'noise/F')
 pedestal = array( 'f', [ 0 ] )
 myT.Branch( 'pedestal', pedestal, 'pedestal/F')
-
+#scurve = array( 'TH1D', [ 0 ] )
+scurve_h = TH1D()
+#sc_h = TH1D()
+#scurve_h.append(sc_h)
+#myT.Branch( 'scurve_h', scurve_h)
 chi2 = array( 'f', [ 0 ] )
 myT.Branch( 'chi2', chi2, 'chi2/F')
 
 
-scanFits = fitScanData(filename)
+#scanFits = fitScanData(filename)
+
 vSum = {}
 vNoise = {}
 vThreshold = {}
 vChi2 = {}
 vComparison = {}
 vPedestal = {}
+vScurves = []
+
+vthr_list = []
+trim_list = []
+trimrange_list = []
 def overlay_fit(VFAT, CH):
     Scurve = TH1D('Scurve','Scurve for VFAT %i channel %i;VCal [DAC units]'%(VFAT, CH),255,-0.5,254.5)
+    strip = lookup_table[VFAT][CH]
     for event in inF.scurveTree:
         if (event.vfatN == VFAT) and (event.vfatCH == CH):
             Scurve.Fill(event.vcal, event.Nhits)
@@ -69,50 +112,77 @@ def overlay_fit(VFAT, CH):
     fitTF1.SetParameter(1, param1)
     fitTF1.SetParameter(2, param2)
     canvas = TCanvas('canvas', 'canvas', 500, 500)
+    gStyle.SetOptStat(1111111)
     Scurve.Draw()
     fitTF1.Draw('SAME')
     canvas.Update()
-    canvas.SaveAs('Fit_Overlay_VFAT%i_Channel%i.png'%(VFAT, CH))
+    canvas.SaveAs('Fit_Overlay_VFAT%i_Strip%i.png'%(VFAT, strip))
     return
 
 
 
 for i in range(0,24):
-    vSum[i] = TH2D('vSum%i'%i,'vSum%i;Channel;VCal [DAC units]'%i,128,-0.5,127.5,256,-0.5,255.5)
+    vScurves.append([])
+    vthr_list.append([])
+    trim_list.append([])
+    trimrange_list.append([])
+    vSum[i] = TH2D('vSum%i'%i,'vSum%i;Strip;VCal [DAC units]'%i,128,-0.5,127.5,256,-0.5,255.5)
     vNoise[i] = TH1D('Noise%i'%i,'Noise%i;Noise [DAC units]'%i,35,-0.5,34.5)
     vPedestal[i] = TH1D('Pedestal%i'%i,'Pedestal%i;Pedestal [DAC units]'%i,256,-0.5,255.5)
     vThreshold[i] = TH1D('Threshold%i'%i,'Threshold%i;Threshold [DAC units]'%i,60,-0.5,299.5)
     vChi2[i] = TH1D('ChiSquared%i'%i,'ChiSquared%i;Chi2'%i,100,-0.5,999.5)
     vComparison[i] = TH2D('vComparison%i'%i,'Parameter Spread %i;Threshold [DAC units];Noise [DAC units]'%i,60,-0.5,299.5,70,-0.5,34.5)
+    for ch in range (0,128):
+        vScurves[i].append(TH1D('Scurve_vfat_%i_channel_%i'%(i,ch),'Scurve_vfat_%i_channel_%i;VCal [DAC units]'%(i,ch),256,-0.5,255.5))
+        vthr_list[i].append(0)
+        trim_list[i].append(0)
+        trimrange_list[i].append(0)
+        pass
     pass
+scanFits = fitScanData(filename)
+
+
+
 
 for event in inF.scurveTree:
-    vSum[event.vfatN].Fill(event.vfatCH,event.vcal,event.Nhits)
-    if event.vcal == 1:
-        vfatN[0] = event.vfatN
-        vfatCH[0] = event.vfatCH
-        trimRange[0] = event.trimRange
-        vthr[0] = event.vthr
-        trimDAC[0] = event.trimDAC
-        threshold[0] = scanFits[0][event.vfatN][event.vfatCH]
-        noise[0] = scanFits[1][event.vfatN][event.vfatCH]
-        pedestal[0] = scanFits[2][event.vfatN][event.vfatCH]
-        chi2[0] = scanFits[3][event.vfatN][event.vfatCH]
-        vNoise[event.vfatN].Fill((scanFits[1][event.vfatN][event.vfatCH]))
-        vThreshold[event.vfatN].Fill((scanFits[0][event.vfatN][event.vfatCH]))
-        Chi2 = scanFits[3][event.vfatN][event.vfatCH]
-        vChi2[event.vfatN].Fill(Chi2)
-        vPedestal[event.vfatN].Fill((scanFits[2][event.vfatN][event.vfatCH]))
-        vComparison[event.vfatN].Fill(scanFits[0][event.vfatN][event.vfatCH], scanFits[1][event.vfatN][event.vfatCH])
+    strip = lookup_table[event.vfatN][event.vfatCH]
+    vSum[event.vfatN].Fill(strip,event.vcal,event.Nhits)
+    vScurves[event.vfatN][event.vfatCH].Fill(event.vcal, event.Nhits)
+    vthr_list[event.vfatN][event.vfatCH] = event.vthr
+    trim_list[event.vfatN][event.vfatCH] = event.trimDAC
+    trimrange_list[event.vfatN][event.vfatCH] = event.trimRange
+    pass
+for vfat in range (0,24):
+    for CH in range (0, 128):
+        strip = lookup_table[vfat][CH]
+            #Filling the Branches
+        vfatN[0] = vfat
+        vfatCH[0] = CH
+        vfatstrip[0] = strip
+        trimRange[0] = trimrange_list[vfat][CH] 
+        vthr[0] = vthr_list[vfat][CH]
+        trimDAC[0] = trim_list[vfat][CH]
+        threshold[0] = scanFits[0][vfat][CH]
+        noise[0] = scanFits[1][vfat][CH]
+        pedestal[0] = scanFits[2][vfat][CH]
+        chi2[0] = scanFits[3][vfat][CH]
+        scurve_h = vScurves[vfat][CH]
+        #Filling the arrays for plotting later
+        vNoise[vfat].Fill((scanFits[1][vfat][CH]))
+        vThreshold[vfat].Fill((scanFits[0][vfat][CH]))
+        Chi2 = scanFits[3][vfat][CH]
+        vChi2[vfat].Fill(Chi2)
+        vPedestal[vfat].Fill((scanFits[2][vfat][CH]))
+        vComparison[vfat].Fill(scanFits[0][vfat][CH], scanFits[1][vfat][CH])
         if options.drawbad:
-            if (Chi2 > 1000.0):
-                overlay_fit(event.vfatN, event.vfatCH)
+            if (Chi2 > 1000.0 or Chi2 < 1.0):
+                overlay_fit(vfat, CH)
                 print "Chi2 is, %d"%(Chi2)
                 pass
             pass
         myT.Fill()
-        pass
-    pass 
+        pass 
+    pass
 
 outF.cd()
 
@@ -137,7 +207,7 @@ for i in range(0,24):
     canv_comp.Update()
     vComparison[i].Write()
     pass
-canv_comp.SaveAs('ParameterSpread.png')
+canv_comp.SaveAs(filename[:-5]+'_ParameterSpread.png')
 
 
 canv_thresh = TCanvas('canv','canv',500*8,500*3)
@@ -150,7 +220,7 @@ for i in range(0,24):
     canv_thresh.Update()
     vThreshold[i].Write()
     pass
-canv_thresh.SaveAs('FitThreshSummary.png')
+canv_thresh.SaveAs(filename[:-5]+'_FitThreshSummary.png')
 
 canv_Pedestal = TCanvas('canv','canv',500*8,500*3)
 canv_Pedestal.Divide(8,3)
@@ -162,7 +232,7 @@ for i in range(0,24):
     canv_Pedestal.Update()
     vPedestal[i].Write()
     pass
-canv_Pedestal.SaveAs('FitPedestalSummary.png')
+canv_Pedestal.SaveAs(filename[:-5]+'_FitPedestalSummary.png')
 
 
 canv_noise = TCanvas('canv','canv',500*8,500*3)
@@ -175,7 +245,7 @@ for i in range(0,24):
     vNoise[i].Write()
     pass
 canv_noise.SetLogy()
-canv_noise.SaveAs('FitNoiseSummary.png')
+canv_noise.SaveAs(filename[:-5]+'_FitNoiseSummary.png')
 
 canv_Chi2 = TCanvas('canv','canv',500*8,500*3)
 canv_Chi2.Divide(8,3)
@@ -188,7 +258,7 @@ for i in range(0,24):
     vChi2[i].Write()
     pass
 canv_Chi2.SetLogy()
-canv_Chi2.SaveAs('FitChi2Summary.png')
+canv_Chi2.SaveAs(filename[:-5]+'_FitChi2Summary.png')
 
 outF.Write()
 outF.Close()
