4bb2429a080e254d6fdf7f3dbf4ae38bd2526f92
Jared Sturdy <sturdy@cern.ch>
2017-03-22 23:13:43 +0100

    moved chamber config info to separate module

---
+++ chamberInfo.py
@@ -0,0 +1,39 @@
+
+chamber_config = {
+    0:"GEMINI01mL1",
+    1:"GEMINI01mL2",
+    2:"GEMINI27mL1",
+    3:"GEMINI27mL2",
+    4:"GEMINI28mL1",
+    5:"GEMINI28mL2",
+    6:"GEMINI29mL1",
+    7:"GEMINI29mL2",
+    8:"GEMINI30mL1",
+    9:"GEMINI30mL2"
+    }
+
+chamber_config = {
+    0:"SC1L1",
+    1:"SC1L2",
+    2:"SC27L1",
+    3:"SC27L2",
+    4:"SC28L1",
+    5:"SC28L2",
+    6:"SC29L1",
+    7:"SC29L2",
+    8:"SC30L1",
+    9:"SC30L2"
+    }
+
+GEBtype = {
+    0:"short",
+    1:"short",
+    2:"short",
+    3:"short",
+    4:"long",
+    5:"long",
+    6:"short",
+    7:"short",
+    8:"long",
+    9:"long"
+    }
