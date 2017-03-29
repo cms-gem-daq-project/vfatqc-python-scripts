a0ff51540d432fb4e398febe24a41d038a6d123e
Cameron Bravo <bravo@srv-s2g18-33-01.cms>
2017-03-29 11:00:20 +0200

    Changed how linking is made to make it safer

--- run_scans.py
+++ run_scans.py
@@ -44,7 +44,7 @@ def launchTestsArgs(tool, slot, link, chamber,vt1=None,vt2=0,perchannel=False,tr
     dirPath = "%s/%s/%s/z%f/"%(dataPath,chamber_config[link],scanType,ztrim)
     setupCmds.append( ["mkdir","-p",dirPath+startTime] )
     setupCmds.append( ["unlink",dirPath+"current"] )
-    setupCmds.append( ["ln","-s",dirPath+startTime,dirPath+"current"] )
+    setupCmds.append( ["ln","-s",startTime,dirPath+"current"] )
     dirPath = dirPath+startTime
     cmd.append("--ztrim=%f"%(ztrim))
     if vt1 in range(256):
