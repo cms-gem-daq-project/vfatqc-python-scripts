0e85af33f751845757f159fb4cab326b26572e59
Jared Sturdy <sturdy@cern.ch>
2017-05-12 20:54:53 +0200

    added fast and ultra latency scans

---
+++ ultraLatency.py
@@ -0,0 +1,146 @@
+#!/bin/env python
+"""
+Script to take latency data using OH ultra scans
+By: Jared Sturdy  (sturdy@cern.ch)
+    Cameron Bravo (c.bravo@cern.ch)
+"""
+
+import sys, os, random, time
+from array import array
+
+from gempython.tools.optohybrid_user_functions_uhal import *
+from gempython.tools.vfat_user_functions_uhal import *
+
+from qcoptions import parser
+
+parser.add_option("--vt1", type="int", dest="vt1",
+                  help="VThreshold1 DAC value for all VFATs", metavar="vt1", default=100)
+parser.add_option("--mspl", type="int", dest="mspl",
+                  help="VThreshold1 DAC value for all VFATs", metavar="mspl", default=1)
+parser.add_option("--vt2", type="int", dest="vt2", default=0,
+                  help="Specify VT2 to use", metavar="vt2")
+parser.add_option("--filename", type="string", dest="filename", default="LatencyData_Trimmed.root",
+                  help="Specify Output Filename", metavar="filename")
+parser.add_option("--internal", action="store_true", dest="internal",
+                  help="Run a latency scan using the internal calibration pulse", metavar="internal")
+
+parser.set_defaults(scanmin=153,scanmax=172,nevts=500)
+
+(options, args) = parser.parse_args()
+
+if options.scanmin not in range(256) or options.scanmax not in range(256) or not (options.scanmax > options.scanmin):
+    print "Invalid scan parameters specified [min,max] = [%d,%d]"%(options.scanmin,options.scanmax)
+    print "Scan parameters must be in range [0,255] and min < max"
+    exit(1)
+
+if options.vt2 not in range(256):
+    print "Invalid VT2 specified: %d, must be in range [0,255]"%(options.vt2)
+    exit(1)
+
+if options.debug:
+    uhal.setLogLevelTo( uhal.LogLevel.INFO )
+else:
+    uhal.setLogLevelTo( uhal.LogLevel.ERROR )
+
+from ROOT import TFile,TTree
+filename = options.filename
+myF = TFile(filename,'recreate')
+myT = TTree('latTree','Tree Holding CMS GEM Latency Data')
+
+Nev = array( 'i', [ 0 ] )
+Nev[0] = options.nevts
+myT.Branch( 'Nev', Nev, 'Nev/I' )
+vth = array( 'i', [ 0 ] )
+myT.Branch( 'vth', vth, 'vth/I' )
+vth1 = array( 'i', [ 0 ] )
+myT.Branch( 'vth1', vth1, 'vth1/I' )
+vth2 = array( 'i', [ 0 ] )
+myT.Branch( 'vth2', vth2, 'vth2/I' )
+vth2[0] = options.vt2
+lat = array( 'i', [ 0 ] )
+myT.Branch( 'lat', lat, 'lat/I' )
+Nhits = array( 'i', [ 0 ] )
+myT.Branch( 'Nhits', Nhits, 'Nhits/I' )
+vfatN = array( 'i', [ 0 ] )
+myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
+vfatCH = array( 'i', [ 0 ] )
+myT.Branch( 'vfatCH', vfatCH, 'vfatCH/I' )
+link = array( 'i', [ 0 ] )
+myT.Branch( 'link', link, 'link/I' )
+link[0] = options.gtx
+utime = array( 'i', [ 0 ] )
+myT.Branch( 'utime', utime, 'utime/I' )
+
+import subprocess,datetime,time
+utime[0] = int(time.time())
+startTime = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M")
+print startTime
+Date = startTime
+
+ohboard = getOHObject(options.slot,options.gtx,options.shelf,options.debug)
+
+LATENCY_MIN = options.scanmin
+LATENCY_MAX = options.scanmax
+
+N_EVENTS = Nev[0]
+mask     = 0
+
+try:
+    writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x37, mask)
+    writeAllVFATs(ohboard, options.gtx, "ContReg2",    ((options.mspl)<<4))
+    # writeAllVFATs(ohboard, options.gtx, "VThreshold1", options.vt1, mask)
+    writeAllVFATs(ohboard, options.gtx, "VThreshold2", options.vt2, mask)
+
+    mode = scanmode.LATENCY
+
+    if options.internal:
+        setTriggerSource(ohboard,options.gtx,0x1)
+        # sendL1A(        ohboard, options.gtx, interval=250, number=0)
+        sendL1ACalPulse(ohboard, options.gtx, delay=20, interval=400, number=0)
+        chanReg = ((1&0x1) << 6)|((0&0x1) << 5)|(0&0x1f)
+        writeAllVFATs(ohboard, options.gtx, "VFATChannels.ChanReg0", chanReg, mask)
+        writeAllVFATs(ohboard, options.gtx, "VCal",     250, mask)
+    else:
+        # setTriggerSource(ohboard,options.gtx,0x0) # GTX
+        setTriggerSource(ohboard,options.gtx,0x5) # GBT
+        pass
+
+    scanBase = "GEM_AMC.OH.OH%d.ScanController.ULTRA"%(options.gtx)
+    if (readRegister(ohboard,"%s.MONITOR.STATUS"%(scanBase)) > 0):
+        print "Scan was already running, resetting module"
+        writeRegister(ohboard,"%s.RESET"%(scanBase),0x1)
+        time.sleep(0.1)
+        pass
+    configureScanModule(ohboard, options.gtx, mode, mask,
+                        scanmin=LATENCY_MIN, scanmax=LATENCY_MAX,
+                        numtrigs=int(options.nevts),
+                        useUltra=True, debug=True)
+    printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
+    sys.stdout.flush()
+    startScanModule(ohboard, options.gtx, useUltra=True, debug=options.debug)
+    printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
+    sys.stdout.flush()
+    scanData = getUltraScanResults(ohboard, options.gtx, LATENCY_MAX - LATENCY_MIN + 1, options.debug)
+    sys.stdout.flush()
+    for i in range(0,24):
+        vfatN[0] = i
+        dataNow = scanData[i]
+        for VC in range(LATENCY_MAX-LATENCY_MIN+1):
+            lat[0]   = int((dataNow[VC] & 0xff000000) >> 24)
+            Nhits[0] = int(dataNow[VC] & 0xffffff)
+            print "{0} {1} 0x{2:x} {3} {4}".format(i,VC,dataNow[VC],lat[0],Nhits[0])
+            myT.Fill()
+            pass
+        pass
+    myT.AutoSave("SaveSelf")
+    writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x36, mask)
+    if options.internal:
+        stopLocalT1(ohboard, options.gtx)
+        pass
+except Exception as e:
+    myT.AutoSave("SaveSelf")
+    print "An exception occurred", e
+finally:
+    myF.cd()
+    myT.Write()
+    myF.Close()
