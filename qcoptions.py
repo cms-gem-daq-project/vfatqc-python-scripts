9424d9026656cd221249fa09cf110f143fb34759
Brian Dorney <brian.l.dorney@cern.ch>
2017-07-21 09:18:59 +0200

    updated default mspl and ztrim values in qcoptions

--- qcoptions.py
+++ qcoptions.py
@@ -1,7 +1,7 @@
 from gempython.utils.standardopts import parser
 
-parser.add_option("--mspl", type="int", dest = "MSPL", default = 4,
-                  help="Specify MSPL.  Must be in the range 1-8 (default is 4)", metavar="MSPL")
+parser.add_option("--mspl", type="int", dest = "MSPL", default = 1,
+                  help="Specify MSPL.  Must be in the range 1-8 (default is 1)", metavar="MSPL")
 parser.add_option("--nevts", type="int", dest="nevts",
                   help="Number of events to count at each scan point", metavar="nevts", default=1000)
 parser.add_option("--scanmin", type="int", dest="scanmin",
@@ -10,5 +10,5 @@ parser.add_option("--scanmax", type="int", dest="scanmax",
                   help="Maximum value of scan parameter", metavar="scanmax", default=254)
 parser.add_option("--vfatmask", type="int", dest="vfatmask", 
                   help="VFATs to be masked in scan & analysis applications (e.g. 0xFFFFF masks all VFATs)", metavar="vfatmask", default=0x0)
-parser.add_option("--ztrim", type="float", dest="ztrim", default=0.0,
+parser.add_option("--ztrim", type="float", dest="ztrim", default=4.0,
                   help="Specify the p value of the trim", metavar="ztrim")
