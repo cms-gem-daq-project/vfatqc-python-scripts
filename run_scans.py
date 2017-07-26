93bd8aac3c1be9dec917dda0d5d95cae7c387d30
EklavyaFCB <eklavya1994@gmail.com>
2017-07-26 13:31:04 +0200

    Forgot a comma in params

--- run_scans.py
+++ run_scans.py
@@ -3,7 +3,7 @@
 def launchTests(args):
   return launchTestsArgs(*args)
 
-def launchTestsArgs(tool, shelf, slot, link, chamber, scanmin, scanmax, nevts, stepSize=1
+def launchTestsArgs(tool, shelf, slot, link, chamber, scanmin, scanmax, nevts, stepSize=1,
                     vt1=None,vt2=0,mspl=None,perchannel=False,trkdata=False,ztrim=4.0,
                     config=False,amc13local=False,t3trig=False, randoms=0, throttle=0,
                     internal=False):
