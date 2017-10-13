88745b18919bc866cc37692791872e1e2e27e175
Brian Dorney <brian.l.dorney@cern.ch>
2017-10-13 16:28:56 +0200

    Bare bones tools to take v3 electronics qc data

--- qcoptions.py
+++ qcoptions.py
@@ -1,13 +1,19 @@
 from gempython.utils.standardopts import parser
 
+parser.add_option("--L1Atime", type="int", dest = "L1Atime", default = 250,
+                  help="Specify time between L1As in bx", metavar="L1Atime")
 parser.add_option("--mspl", type="int", dest = "MSPL", default = 4,
                   help="Specify MSPL. Must be in the range 1-8 (default is 4)", metavar="MSPL")
 parser.add_option("--nevts", type="int", dest="nevts",
                   help="Number of events to count at each scan point", metavar="nevts", default=1000)
+parser.add_option("--pulseDelay", type="int", dest = "pDel", default = 40,
+                  help="Specify time of pulse before L1A in bx", metavar="pDel")
 parser.add_option("--scanmin", type="int", dest="scanmin",
                   help="Minimum value of scan parameter", metavar="scanmin", default=0)
 parser.add_option("--scanmax", type="int", dest="scanmax",
                   help="Maximum value of scan parameter", metavar="scanmax", default=254)
+parser.add_option("--stepSize", type="int", dest="stepSize", 
+                  help="Supply a step size to the scan from scanmin to scanmax", metavar="stepSize", default=1)
 parser.add_option("--vfatmask", type="int", dest="vfatmask",
                   help="VFATs to be masked in scan & analysis applications (e.g. 0xFFFFF masks all VFATs)", metavar="vfatmask", default=0x0)
 parser.add_option("--ztrim", type="float", dest="ztrim", default=4.0,
--- ultraThreshold.py
+++ ultraThreshold.py
@@ -3,38 +3,41 @@
 Script to take VT1 data using OH ultra scans
 By: Cameron Bravo (c.bravo@cern.ch)
     Jared Sturdy  (sturdy@cern.ch)
-
-Modified By:
     Brian Dorney (brian.l.dorney@cern.ch)
 """
 
 import sys, os, random, time
 from array import array
+from ctypes import *
 
-from gempython.tools.optohybrid_user_functions_uhal import *
-from gempython.tools.vfat_user_functions_uhal import *
+#from gempython.tools.optohybrid_user_functions_uhal import *
+#from gempython.tools.vfat_user_functions_uhal import *
 
 from gempython.vfatqc.qcoptions import parser
 
-parser.add_option("--vt2", type="int", dest="vt2", default=0,
-                  help="Specify VT2 to use", metavar="vt2")
+parser.add_option("--chMin", type="int", dest = "chMin", default = 0,
+                  help="Specify minimum channel number to scan", metavar="chMin")
+parser.add_option("--chMax", type="int", dest = "chMax", default = 127,
+                  help="Specify maximum channel number to scan", metavar="chMax")
 parser.add_option("-f", "--filename", type="string", dest="filename", default="VThreshold1Data_Trimmed.root",
                   help="Specify Output Filename", metavar="filename")
 parser.add_option("--perchannel", action="store_true", dest="perchannel",
                   help="Run a per-channel VT1 scan", metavar="perchannel")
 parser.add_option("--trkdata", action="store_true", dest="trkdata",
                   help="Run a per-VFAT VT1 scan using tracking data (default is to use trigger data)", metavar="trkdata")
+parser.add_option("--vt2", type="int", dest="vt2", default=0,
+                  help="Specify VT2 to use", metavar="vt2")
 
 (options, args) = parser.parse_args()
 
-if options.vt2 not in range(256):
-    print "Invalid VT2 specified: %d, must be in range [0,255]"%(options.vt2)
-    exit(1)
-
-if options.debug:
-    uhal.setLogLevelTo( uhal.LogLevel.INFO )
-else:
-    uhal.setLogLevelTo( uhal.LogLevel.ERROR )
+#if options.vt2 not in range(256):
+#    print "Invalid VT2 specified: %d, must be in range [0,255]"%(options.vt2)
+#    exit(1)
+#
+#if options.debug:
+#    uhal.setLogLevelTo( uhal.LogLevel.INFO )
+#else:
+#    uhal.setLogLevelTo( uhal.LogLevel.ERROR )
 
 import ROOT as r
 filename = options.filename
@@ -45,24 +48,18 @@ startTime = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M")
 print startTime
 Date = startTime
 
-# Setup the output TTree
-from treeStructure import gemTreeStructure
-gemData = gemTreeStructure('thrTree','Tree Holding CMS GEM VT1 Data')
-gemData.setDefaults(options, int(time.time()))
+from rpcService import *
+from gempython.gemplotting.mapping.amcInfo import ctp7Params 
+rpc_connect(ctp7Params.cardLocation[(options.shelf, options.slot)])
+print 'opened connection'
+#ohboard = getOHObject(options.slot,options.gtx,options.shelf,options.debug)
 
-from gempython.tools.amc_user_functions_uhal import *
-amcBoard = getAMCObject(options.slot, options.shelf, options.debug)
-printSystemSCAInfo(amcBoard, options.debug)
-printSystemTTCInfo(amcBoard, options.debug)
+#THRESH_MIN = 0
+#THRESH_MAX = 254
 
-ohboard = getOHObject(options.slot,options.gtx,options.shelf,options.debug)
-
-THRESH_MIN = 0
-THRESH_MAX = 254
-
-N_EVENTS = options.nevts
-CHAN_MIN = 0
-CHAN_MAX = 128
+# N_EVENTS = options.nevts
+CHAN_MIN = options.chMin
+CHAN_MAX = options.chMax + 1
 if options.debug:
     CHAN_MAX = 5
     pass
@@ -70,137 +67,116 @@ if options.debug:
 mask = options.vfatmask
 
 try:
-    writeAllVFATs(ohboard, options.gtx, "Latency",     0, mask)
-    writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x37, mask)
-    writeAllVFATs(ohboard, options.gtx, "VThreshold2", options.vt2, mask)
+    #writeAllVFATs(ohboard, options.gtx, "Latency",     0, mask)
+    #writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x37, mask)
+    #writeAllVFATs(ohboard, options.gtx, "VThreshold2", options.vt2, mask)
 
-    vals = readAllVFATs(ohboard, options.gtx, "CalPhase",   0x0)
-    calPhasevals = dict(map(lambda slotID: (slotID, bin(vals[slotID]).count("1")),
-                         range(0,24)))
-    vals = readAllVFATs(ohboard, options.gtx, "ContReg2",    0x0)
-    msplvals =  dict(map(lambda slotID: (slotID, (1+(vals[slotID]>>4)&0x7)),
-                         range(0,24)))
-    vals = readAllVFATs(ohboard, options.gtx, "ContReg3",    0x0)
-    trimRangevals = dict(map(lambda slotID: (slotID, (0x07 & vals[slotID])),
-                         range(0,24)))
-    vals = readAllVFATs(ohboard, options.gtx, "Latency",    0x0)
-    latvals = dict(map(lambda slotID: (slotID, vals[slotID]&0xff),
-                         range(0,24)))
-    vfatIDvals = getAllChipIDs(ohboard, options.gtx, 0x0)
-    vals  = readAllVFATs(ohboard, options.gtx, "VThreshold2", 0x0)
-    vt2vals =  dict(map(lambda slotID: (slotID, vals[slotID]&0xff),
-                        range(0,24)))
-    vthvals =  dict(map(lambda slotID: (slotID, vt2vals[slotID]-vt2vals[slotID]),
-                        range(0,24)))
-
-    trgSrc = getTriggerSource(ohboard,options.gtx)
+    #trgSrc = getTriggerSource(ohboard,options.gtx)
     if options.perchannel:
-        setTriggerSource(ohboard,options.gtx,0x1)
-        gemData.mode[0] = scanmode.THRESHCH
-        sendL1A(ohboard, options.gtx, interval=250, number=0)
+        # Configure TTC
+        if 0 == ttcGenConf(options.L1Atime, options.pDel):
+            print "TTC configured successfully"
+        else:
+            print "TTC configuration failed"
+            sys.exit(os.EX_CONFIG)
+        
+        #setTriggerSource(ohboard,options.gtx,0x1)
+        #mode[0] = scanmode.THRESHCH
+        #sendL1A(ohboard, options.gtx, interval=250, number=0)
+    
+        scanDataSizeVFAT = (options.scanmax-options.scanmin+1)/options.stepSize
+        scanDataSizeNet = scanDataSizeVFAT * 24
+        scanData = (c_uint32 * scanDataSizeNet)()
+        for chan in range(CHAN_MIN,CHAN_MAX):
+            vfatCH[0] = chan
+            print "Channel #"+str(chan)
+            #configureScanModule(ohboard, options.gtx, mode[0], mask, channel=scCH,
+            #                    scanmin=THRESH_MIN, scanmax=THRESH_MAX,
+            #                    numtrigs=int(N_EVENTS),
+            #                    useUltra=True, debug=options.debug)
+            #printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
 
-        l1AInterval = readRegister(ohboard,"GEM_AMC.OH.OH%d.T1Controller.INTERVAL"%(options.gtx),options.debug)
-        
-        for scCH in range(CHAN_MIN,CHAN_MAX):
-            print "Channel #"+str(scCH)
-        
-            # Get trimDAC vals for output TTree
-            vals = readAllVFATs(ohboard, options.gtx, "VFATChannels.ChanReg%d"%(scCH),   0x0)
-            trimDACvals = dict(map(lambda slotID: (slotID, 0x1f & vals[slotID]),
-                                 range(0,24)))
-        
-            configureScanModule(ohboard, options.gtx, gemData.getMode(), mask, channel=scCH,
-                                scanmin=THRESH_MIN, scanmax=THRESH_MAX,
-                                numtrigs=int(N_EVENTS),
-                                useUltra=True, debug=options.debug)
-            printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
+            #startScanModule(ohboard, options.gtx, useUltra=True, debug=options.debug)
+            #scanData = getUltraScanResults(ohboard, options.gtx, THRESH_MAX - THRESH_MIN + 1, options.debug)
+            rpcResp = genScan(  options.nevts, options.gtx,
+                            options.scanmin,options.scanmax,options.stepSize,
+                            chan,1,options.vfatmask,"THR_ARM_DAC",scanData)
 
-            startScanModule(ohboard, options.gtx, useUltra=True, debug=options.debug)
-            scanData = getUltraScanResults(ohboard, options.gtx, THRESH_MAX - THRESH_MIN + 1, options.debug)
+            if rpcResp != 0:
+                print("threshold scan for channel %i failed"%chan)
+                sys.exit(os.EX_SOFTWARE)
+            
             sys.stdout.flush()
             for vfat in range(0,24):
             	if (mask >> vfat) & 0x1: continue
-                dataNow      = scanData[vfat]
-                for VC in range(THRESH_MAX-THRESH_MIN+1):
-                    vth1  = int((dataNow[VC] & 0xff000000) >> 24)
-                    Nhits = int(dataNow[VC] & 0xffffff)
-                    gemData.fill(
-                           calPhase = calPhasevals[vfat],
-                           l1aTime = l1AInterval,
-                           latency = latvals[vfat],
-                           mspl = msplvals[vfat],
-                           Nhits = Nhits, 
-                           trimDAC = trimDACvals[vfat],
-                           trimRange = trimRangevals[vfat],
-                           vfatCH = scCH,
-                           vfatID = vfatIDvals[vfat],
-                           vfatN = vfat,
-                           vth = vthvals[vfat],
-                           vth1 = vth1,
-                           vth2 = vt2vals[vfat]
-                         )
-                    pass
+                vfatN[0] = vfat
+                #dataNow      = scanData[i]
+                #trimRange[0] = (0x07 & readVFAT(ohboard,options.gtx, i,"ContReg3"))
+                for threshDAC in range(vfat*scanDataSizeVFAT,(vfat+1)*scanDataSizeVFAT,options.stepSize):
+                    try:
+                        #vth1[0]  = int((dataNow[VC] & 0xff000000) >> 24)
+                        #vth[0]   = vth2[0] - vth1[0]
+                        #Nhits[0] = int(dataNow[VC] & 0xffffff)
+                        if vfat == 0:
+                            vth1[0] = threshDAC
+                        else:
+                            vth1[0] = threshDAC - vfat*scanDataSizeVFAT
+                        Nev[0] = scanData[threshDAC] & 0xffff
+                        Nhits[0] = (scanData[threshDAC]>>16) & 0xffff
+                        print vfat,chan,threshDAC,vth1[0],Nhits[0],Nev[0]
+                    except IndexError:
+                        print 'Unable to index data for channel %i'%chan
+                        print scanData[threshDAC]
+                        vth1[0]  = -99
+                        Nhits[0] = -99
+                    finally:
+                        myT.Fill()
                 pass
             gemData.autoSave()
             pass
 
-        setTriggerSource(ohboard,options.gtx,trgSrc)
-        stopLocalT1(ohboard, options.gtx)
+        #setTriggerSource(ohboard,options.gtx,trgSrc)
+        #stopLocalT1(ohboard, options.gtx)
         pass
-    else:
-        if options.trkdata:
-            setTriggerSource(ohboard,options.gtx,0x1)
-            gemData.mode[0] = scanmode.THRESHTRK
-            sendL1A(ohboard, options.gtx, interval=250, number=0)
-    
-            l1AInterval = readRegister(ohboard,"GEM_AMC.OH.OH%d.T1Controller.INTERVAL"%(options.gtx),options.debug)
-        else:
-            gemData.mode[0] = scanmode.THRESHTRG
-            pass
-        configureScanModule(ohboard, options.gtx, gemData.getMode(), mask,
-                            scanmin=THRESH_MIN, scanmax=THRESH_MAX,
-                            numtrigs=int(N_EVENTS),
-                            useUltra=True, debug=options.debug)
-        printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
+    #else:
+    #    if options.trkdata:
+    #        setTriggerSource(ohboard,options.gtx,0x1)
+    #        mode[0] = scanmode.THRESHTRK
+    #        sendL1A(ohboard, options.gtx, interval=250, number=0)
+    #    else:
+    #        mode[0] = scanmode.THRESHTRG
+    #        pass
+    #    configureScanModule(ohboard, options.gtx, mode[0], mask,
+    #                        scanmin=THRESH_MIN, scanmax=THRESH_MAX,
+    #                        numtrigs=int(N_EVENTS),
+    #                        useUltra=True, debug=options.debug)
+    #    printScanConfiguration(ohboard, options.gtx, useUltra=True, debug=options.debug)
 
-        startScanModule(ohboard, options.gtx, useUltra=True, debug=options.debug)
-        scanData = getUltraScanResults(ohboard, options.gtx, THRESH_MAX - THRESH_MIN + 1, options.debug)
-        sys.stdout.flush()
-        for i in range(0,24):
-            if (mask >> i) & 0x1: continue
-            vfatN[0]     = i
-            vfatID[0]    = getChipID(ohboard, options.gtx, i, options.debug)
-            dataNow      = scanData[i]
-            trimRange[0] = (0x07 & readVFAT(ohboard,options.gtx, i,"ContReg3"))
-            for VC in range(THRESH_MAX-THRESH_MIN+1):
-                vth1  = int((dataNow[VC] & 0xff000000) >> 24)
-                Nhits = int(dataNow[VC] & 0xffffff)
-                gemData.fill(
-                       calPhase = calPhasevals[vfat],
-                       l1aTime = l1AInterval,
-                       latency = latvals[vfat],
-                       mspl = msplvals[vfat],
-                       Nhits = Nhits, 
-                       trimRange = trimRangevals[vfat],
-                       vfatCH = scCH,
-                       vfatID = vfatIDvals[vfat],
-                       vfatN = vfat,
-                       vth = vthvals[vfat],
-                       vth1 = vth1,
-                       vth2 = vt2vals[vfat]
-                     )
-                pass
-            pass
-        gemData.autoSave()
+    #    startScanModule(ohboard, options.gtx, useUltra=True, debug=options.debug)
+    #    scanData = getUltraScanResults(ohboard, options.gtx, THRESH_MAX - THRESH_MIN + 1, options.debug)
+    #    sys.stdout.flush()
+    #    for i in range(0,24):
+    #        if (mask >> i) & 0x1: continue
+    #        vfatN[0] = i
+    #        dataNow      = scanData[i]
+    #        trimRange[0] = (0x07 & readVFAT(ohboard,options.gtx, i,"ContReg3"))
+    #        for VC in range(THRESH_MAX-THRESH_MIN+1):
+    #            vth1[0]  = int((dataNow[VC] & 0xff000000) >> 24)
+    #            vth[0]   = vth2[0] - vth1[0]
+    #            Nhits[0] = int(dataNow[VC] & 0xffffff)
+    #            myT.Fill()
+    #            pass
+    #        pass
+    #    myT.AutoSave("SaveSelf")
 
-        if options.trkdata:
-            setTriggerSource(ohboard,options.gtx,trgSrc)
-            stopLocalT1(ohboard, options.gtx)
-            pass
-        pass
+    #    if options.trkdata:
+    #        setTriggerSource(ohboard,options.gtx,trgSrc)
+    #        stopLocalT1(ohboard, options.gtx)
+    #        pass
+    #    pass
 
     # Place VFATs back in sleep mode
-    writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x36, mask)
+    #writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x36, mask)
 
 except Exception as e:
     gemData.autoSave()
