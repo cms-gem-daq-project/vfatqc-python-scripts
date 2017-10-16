fc95e6bced05120837c0222bc973aefbd54bfc1c
Brian Dorney <brian.l.dorney@cern.ch>
2017-10-16 19:37:40 +0200

    ultraScurve now has expanded command line options

--- ultraScurve.py
+++ ultraScurve.py
@@ -6,7 +6,7 @@ By: Cameron Bravo (c.bravo@cern.ch) and Brian Dorney (brian.l.dorney@cern.ch)
 
 from array import array
 from ctypes import *
-#from gempython.tools.vfat_user_functions_uhal import *
+from gempython.tools.vfat_user_functions_xhal import *
 
 from gempython.vfatqc.qcoptions import parser
 
@@ -25,10 +25,10 @@ parser.add_option("--latency", type="int", dest = "latency", default = 37,
 
 (options, args) = parser.parse_args()
 
-#if options.MSPL < 1 or options.MSPL > 8:
-#    print 'MSPL must be in the range 1-8'
-#    exit(1)
-#    pass
+if options.MSPL < 1 or options.MSPL > 8:
+    print 'MSPL must be in the range 1-8'
+    exit(1)
+    pass
 #if options.CalPhase < 0 or options.CalPhase > 8:
 #    print 'CalPhase must be in the range 0-8'
 #    exit(1)
@@ -38,30 +38,70 @@ if not (0 <= options.chMin <= options.chMax < 128):
     exit(1)
     pass
 
-#if options.debug:
-#    uhal.setLogLevelTo( uhal.LogLevel.INFO )
-#else:
-#    uhal.setLogLevelTo( uhal.LogLevel.ERROR )
-
 import ROOT as r
 filename = options.filename
 myF = r.TFile(filename,'recreate')
+myT = r.TTree('scurveTree','Tree Holding CMS GEM SCurve Data')
+
+Nev = array( 'i', [ 0 ] )
+Nev[0] = -1
+myT.Branch( 'Nev', Nev, 'Nev/I' )
+
+vcal = array( 'i', [ 0 ] )
+myT.Branch( 'vcal', vcal, 'vcal/I' )
+
+Nhits = array( 'i', [ 0 ] )
+myT.Branch( 'Nhits', Nhits, 'Nhits/I' )
+
+vfatN = array( 'i', [ 0 ] )
+myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
+
+vfatCH = array( 'i', [ 0 ] )
+myT.Branch( 'vfatCH', vfatCH, 'vfatCH/I' )
+
+trimRange = array( 'i', [ 0 ] )
+myT.Branch( 'trimRange', trimRange, 'trimRange/I' )
+
+vthr = array( 'i', [ 0 ] )
+myT.Branch( 'vthr', vthr, 'vthr/I' )
+
+trimDAC = array( 'i', [ 0 ] )
+myT.Branch( 'trimDAC', trimDAC, 'trimDAC/I' )
+
+l1aTime = array( 'i', [ 0 ] )
+myT.Branch( 'l1aTime', l1aTime, 'l1aTime/I' )
+l1aTime[0] = options.L1Atime
+
+mspl = array( 'i', [ 0 ] )
+myT.Branch( 'mspl', mspl, 'mspl/I' )
+mspl[0] = options.MSPL
+
+latency = array( 'i', [ 0 ] )
+myT.Branch( 'latency', latency, 'latency/I' )
+latency[0] = options.latency
+
+pDel = array( 'i', [ 0 ] )
+myT.Branch( 'pDel', pDel, 'pDel/I' )
+pDel[0] = options.pDel
+
+calPhase = array( 'i', [ 0 ] )
+myT.Branch( 'calPhase', calPhase, 'calPhase/I' )
+calPhase[0] = options.CalPhase
+
+link = array( 'i', [ 0 ] )
+myT.Branch( 'link', link, 'link/I' )
+link[0] = options.gtx
+
+utime = array( 'i', [ 0 ] )
+myT.Branch( 'utime', utime, 'utime/I' )
 
 import subprocess,datetime,time
 startTime = datetime.datetime.now().strftime("%Y.%m.%d.%H.%M")
 print startTime
 Date = startTime
 
-from rpcService import *
-from gempython.gemplotting.mapping.amcInfo import ctp7Params 
-rpc_connect(ctp7Params.cardLocation[(options.shelf, options.slot)])
-print 'opened connection'
-#ohboard = getOHObject(options.slot,options.gtx,options.shelf,options.debug)
+vfatBoard = HwVFAT(options.slot, options.gtx, options.shelf, options.debug)
 
-#SCURVE_MIN = 0
-#SCURVE_MAX = 254
-
-# N_EVENTS = options.nevts
 CHAN_MIN = options.chMin
 CHAN_MAX = options.chMax + 1
 if options.debug:
@@ -77,19 +117,16 @@ try:
 
     #print 'Link %i T1 controller status: %i'%(options.gtx,getLocalT1Status(ohboard,options.gtx))
 
-    # Test
-    #configureVFAT3s(options.gtx, options.vfatmask)
-
     # Configure TTC
-    if 0 == ttcGenConf(options.L1Atime, options.pDel):
+    if 0 == vfatBoard.parentOH.parentAMC.ttcGenConf(options.L1Atime, options.pDel):
         print "TTC configured successfully"
     else:
         print "TTC configuration failed"
         sys.exit(os.EX_CONFIG)
 
-    #writeAllVFATs(ohboard, options.gtx, "Latency",    options.latency, mask)
-    #writeAllVFATs(ohboard, options.gtx, "ContReg0", 0x37, mask)
-    #writeAllVFATs(ohboard, options.gtx, "ContReg2",   (options.MSPL - 1) << 4, mask)
+    vfatBoard.setVFATLatencyAll(mask=options.vfatmask, lat=options.latency, debug=options.debug)
+    vfatBoard.setRunModeAll(mask, True, options.debug)
+    vfatBoard.setVFATMSPLAll(mask, options.MSPL, options.debug)
     #writeAllVFATs(ohboard, options.gtx, "CalPhase",  0xff >> (8 - options.CalPhase), mask)
 
     #for vfat in range(0,24):
@@ -114,24 +151,26 @@ try:
         #printScanConfiguration(ohboard, options.gtx, useUltra = True, debug = options.debug)
         #startScanModule(ohboard, options.gtx, useUltra = True, debug = options.debug)
         #scanData = getUltraScanResults(ohboard, options.gtx, SCURVE_MAX - SCURVE_MIN + 1, options.debug)
-        rpcResp = genScan(  options.nevts, options.gtx,
-                            options.scanmin,options.scanmax,options.stepSize,
-                            chan,1,options.vfatmask,"CAL_DAC",scanData)
+        rpcResp = vfatBoard.parentOH.genScan(options.nevts, options.gtx,
+                                             options.scanmin,options.scanmax,options.stepSize,
+                                             chan,1,options.vfatmask,"CAL_DAC",scanData)
 
         if rpcResp != 0:
             print("scurve for channel %i failed"%chan)
             sys.exit(os.EX_SOFTWARE)
-        #else:
-        #    print scanData
-        #    print len(scanData)
-
+        
         for vfat in range(0,24):
             if (mask >> vfat) & 0x1: continue
             vfatN[0] = vfat
             #dataNow = scanData[vfat]
-            #trimRange[0] = (0x07 & readVFAT(ohboard,options.gtx, i,"ContReg3"))
-            #trimDAC[0]   = (0x1f & readVFAT(ohboard,options.gtx, i,"VFATChannels.ChanReg%d"%(scCH)))
-            #vthr[0]      = (0xff & readVFAT(ohboard,options.gtx, i,"VThreshold1"))
+            if vfatBoard.parentOH.parentAMC.fwVersion > 2:
+                trimDAC[0]   = (0x3f & vfatBoard.readVFAT(vfat,"VFAT_CHANNELS.CHANNEL%d.ARM_TRIM_AMPLITUDE"%(chan)))
+                vthr[0]      = (0xff & vfatBoard.readVFAT(vfat,"CFG_THR_ARM_DAC"))
+            else:
+                trimRange[0] = (0x07 & vfatBoard.readVFAT(vfat,"ContReg3"))
+                trimDAC[0]   = (0x1f & vfatBoard.readVFAT(vfat,"VFATChannels.ChanReg%d"%(chan)))
+                vthr[0]      = (0xff & vfatBoard.readVFAT(vfat,"VThreshold1"))
+            
             for vcalDAC in range(vfat*scanDataSizeVFAT,(vfat+1)*scanDataSizeVFAT,options.stepSize):
                 try:
                     #vcal[0]  = int((dataNow[VC] & 0xff000000) >> 24)
@@ -144,8 +183,7 @@ try:
                         vcal[0] = (vfat+1)*scanDataSizeVFAT - vcalDAC
                     Nev[0] = scanData[vcalDAC] & 0xffff
                     Nhits[0] = (scanData[vcalDAC]>>16) & 0xffff
-                    #Nhits[0] = scanData[vcalDAC]
-                    print vfat,chan,vcalDAC,vcal[0],Nhits[0],Nev[0]
+                    #print vfat,chan,vcalDAC,vcal[0],Nhits[0],Nev[0]
                 except IndexError:
                     print 'Unable to index data for channel %i'%chan
                     print scanData[vcalDAC]
@@ -161,7 +199,7 @@ try:
         #sys.stdout.flush()
         #pass
     #stopLocalT1(ohboard, options.gtx)
-    #writeAllVFATs(ohboard, options.gtx, "ContReg0",    0x36, mask)
+    vfatBoard.setRunModeAll(mask, False, options.debug)
 
 except Exception as e:
     gemData.autoSave()
