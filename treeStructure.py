c1532cdd1a442a93c107df929bc497a18c0079c7
Brian Dorney <brian.l.dorney@cern.ch>
2017-10-20 12:11:19 +0200

    Adding common gemTreeStructure class

--- treeStructure.py
+++ treeStructure.py
@@ -2,67 +2,67 @@ import ROOT as r
 
 class gemTreeStructure:
     def __init__(self, name, description="Generic GEM TTree"):
-        myT = r.TTree(name,description)
+        gemTree = r.TTree(name,description)
 
         self.calPhase = array( 'i', [ 0 ] )
-        self.myT.Branch( 'calPhase', calPhase, 'calPhase/I' )
+        self.gemTree.Branch( 'calPhase', self.calPhase, 'calPhase/I' )
         
         self.Dly = array( 'i', [ -1 ] )
-        self.myT.Branch( 'Dly', Dly, 'Dly/I' )
+        self.gemTree.Branch( 'Dly', self.Dly, 'Dly/I' )
         
         self.l1aTime = array( 'i', [ 0 ] )
-        self.myT.Branch( 'l1aTime', l1aTime, 'l1aTime/I' )
+        self.gemTree.Branch( 'l1aTime', self.l1aTime, 'l1aTime/I' )
         
         #self.lat = array( 'i', [ 0 ] )
-        #self.myT.Branch( 'lat', lat, 'lat/I' ) #used by ultraLatency
+        #self.gemTree.Branch( 'lat', lat, 'lat/I' ) #used by ultraLatency
         
         self.latency = array( 'i', [ 0 ] )
-        self.myT.Branch( 'latency', latency, 'latency/I' ) #used by ultraScurve, same physical quantity
+        self.gemTree.Branch( 'latency', self.latency, 'latency/I' ) #used by ultraScurve, same physical quantity
         
         self.link = array( 'i', [ 0 ] )
-        self.myT.Branch( 'link', link, 'link/I' )
+        self.gemTree.Branch( 'link', self.link, 'link/I' )
         
         self.pDel = array( 'i', [ 0 ] )
-        self.myT.Branch( 'pDel', pDel, 'pDel/I' )
+        self.gemTree.Branch( 'pDel', self.pDel, 'pDel/I' )
         
         self.mode = array( 'i', [ 0 ] )
-        self.myT.Branch( 'mode', mode, 'mode/I' )
+        self.gemTree.Branch( 'mode', self.mode, 'mode/I' )
         
         self.mspl = array( 'i', [ -1 ] )
-        self.myT.Branch( 'mspl', mspl, 'mspl/I' )
+        self.gemTree.Branch( 'mspl', self.mspl, 'mspl/I' )
         
         self.Nev = array( 'i', [ 0 ] )
-        self.myT.Branch( 'Nev', Nev, 'Nev/I' )
+        self.gemTree.Branch( 'Nev', self.Nev, 'Nev/I' )
         
         self.Nhits = array( 'i', [ 0 ] )
-        self.myT.Branch( 'Nhits', Nhits, 'Nhits/I' )
+        self.gemTree.Branch( 'Nhits', self.Nhits, 'Nhits/I' )
         
         self.trimDAC = array( 'i', [ 0 ] )
-        self.myT.Branch( 'trimDAC', trimDAC, 'trimDAC/I' )
+        self.gemTree.Branch( 'trimDAC', self.trimDAC, 'trimDAC/I' )
         
         self.trimRange = array( 'i', [ 0 ] )
-        self.myT.Branch( 'trimRange', trimRange, 'trimRange/I' )
+        self.gemTree.Branch( 'trimRange', self.trimRange, 'trimRange/I' )
         
         self.utime = array( 'i', [ 0 ] )
-        self.myT.Branch( 'utime', utime, 'utime/I' )
+        self.gemTree.Branch( 'utime', self.utime, 'utime/I' )
         
         self.vcal = array( 'i', [ 0 ] )
-        self.myT.Branch( 'vcal', vcal, 'vcal/I' )
+        self.gemTree.Branch( 'vcal', self.vcal, 'vcal/I' )
         
         self.vfatCH = array( 'i', [ 0 ] )
-        self.myT.Branch( 'vfatCH', vfatCH, 'vfatCH/I' )
+        self.gemTree.Branch( 'vfatCH', self.vfatCH, 'vfatCH/I' )
         
         self.vfatN = array( 'i', [ -1 ] )
-        self.myT.Branch( 'vfatN', vfatN, 'vfatN/I' )
+        self.gemTree.Branch( 'vfatN', self.vfatN, 'vfatN/I' )
         
         self.vth = array( 'i', [ 0 ] )
-        self.myT.Branch( 'vth', vth, 'vth/I' )
+        self.gemTree.Branch( 'vth', self.vth, 'vth/I' )
         
         #self.vthr = array( 'i', [ 0 ] )
-        #self.myT.Branch( 'vthr', vthr, 'vthr/I' ) # this is vth1, for some reason ultraScurve used different name
+        #self.gemTree.Branch( 'vthr', vthr, 'vthr/I' ) # this is vth1, for some reason ultraScurve used different name
         
         self.vth1 = array( 'i', [ 0 ] )
-        self.myT.Branch( 'vth1', vth1, 'vth1/I' )
+        self.gemTree.Branch( 'vth1', self.vth1, 'vth1/I' )
         
         self.vth2 = array( 'i', [ 0 ] )
-        self.myT.Branch( 'vth2', vth2, 'vth2/I' )
+        self.gemTree.Branch( 'vth2', self.vth2, 'vth2/I' )
