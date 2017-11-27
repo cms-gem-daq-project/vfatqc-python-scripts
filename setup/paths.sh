f0532486fc5aa85f9a64640be170be1339e17303
Jared Sturdy <jsturdy@users.noreply.github.com>
2017-11-27 15:43:08 +0100

    Update paths.sh
    fixing syntax issue with recent update
    * `-n` changed to `-z`
    * all logic happens in the `if` rather than the `else` with an empty `if`

--- setup/paths.sh
+++ setup/paths.sh
@@ -2,35 +2,31 @@
 # echo "Checking paths" 
 
 #export BUILD_HOME=<your path>/cmsgemos/../
-if [[ -n "$BUILD_HOME" ]]; then
-    ; # echo BUILD_HOME $BUILD_HOME
-else
+if [ -z "$BUILD_HOME" ]
+then
     echo "BUILD_HOME not set, please set BUILD_HOME to the directory above the root of your repository"
     echo " (export BUILD_HOME=<your path>/cmsgemos/../) and then rerun this script"
     return
 fi
 
 #export DATA_PATH=/<your>/<data>/<directory>
-if [[ -n "$DATA_PATH" ]]; then
-    ; # echo DATA_PATH $DATA_PATH
-else
+if [ -z "$DATA_PATH" ]
+then
     echo "DATA_PATH not set, please set DATA_PATH to a directory where data files created by scan applications will be written"
     echo " (export DATA_PATH=<your>/<data>/<directory>/) and then rerun this script"
     return
 fi
 
 # Checking GEM_PYTHON_PATH
-if [[ -n "$GEM_PYTHON_PATH" ]]; then
-    ; # echo GEM_PYTHON_PATH $GEM_PYTHON_PATH
-else
+if [ -z "$GEM_PYTHON_PATH" ]
+then
     echo "GEM_PYTHON_PATH not set, please source \$BUILD_HOME/cmsgemos/setup/paths.sh"
     return
 fi
 
 # Checking GEM_PLOTTING_PROJECT
-if [[ -n "$GEM_PLOTTING_PROJECT" ]]; then
-    ; # echo GEM_PLOTTING_PROJECT $GEM_PLOTTING_PROJECT
-else
+if [ -z "$GEM_PLOTTING_PROJECT" ]
+then
     echo "GEM_PLOTTING_PROJECT not set, please source \$BUILD_HOME/gem-plotting-tools/setup/paths.sh"
     return
 fi
