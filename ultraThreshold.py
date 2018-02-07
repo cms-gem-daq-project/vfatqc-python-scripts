5120d3823da3c08cacc71a712006a9f6726a907c
Jared Sturdy <jsturdy@users.noreply.github.com>
2018-02-07 16:57:19 +0100

    Fix missing set of vfatID in non perchannel scans

--- ultraThreshold.py
+++ ultraThreshold.py
@@ -155,7 +155,8 @@ try:
         sys.stdout.flush()
         for i in range(0,24):
             if (mask >> i) & 0x1: continue
-            vfatN[0] = i
+            vfatN[0]     = i
+            vfatID[0]    = getChipID(ohboard, options.gtx, i, options.debug)
             dataNow      = scanData[i]
             trimRange[0] = (0x07 & readVFAT(ohboard,options.gtx, i,"ContReg3"))
             for VC in range(THRESH_MAX-THRESH_MIN+1):
