bafd5421f0bd95a69f64f38fa839e68d47800238
Brian Dorney <brian.l.dorney@cern.ch>
2018-06-11 17:27:26 +0200

    missed an instance of the remove array vthvals

--- ultraThreshold.py
+++ ultraThreshold.py
@@ -135,7 +135,7 @@ try:
                            vfatCH = scCH,
                            vfatID = vfatIDvals[vfat],
                            vfatN = vfat,
-                           vth = vthvals[vfat],
+                           vth = vt2vals[vfat] - vth1,
                            vth1 = vth1,
                            vth2 = vt2vals[vfat]
                          )
