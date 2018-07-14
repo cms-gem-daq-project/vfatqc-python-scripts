dde6547dbead39c1c91fd3853d69b970b4ce5b7c
Brian Dorney <brian.l.dorney@cern.ch>
2018-07-14 08:34:39 +0200

    new tree class gemDacCalTreeStructure added and inheritence from base tree class provided

--- treeStructure.py
+++ treeStructure.py
@@ -4,7 +4,7 @@ from gempython.tools.optohybrid_user_functions_uhal import scanmode
 import sys,os
 import ROOT as r
 
-class gemTreeStructure:
+class gemGenericTree(object):
     def __init__(self, name, description="Generic GEM TTree",scanmode=-1):
         """
         scanmode    scan type, e.g. scanmode.<TYPE> parameter
@@ -13,6 +13,150 @@ class gemTreeStructure:
         """
 
         self.gemTree = r.TTree(name,description)
+
+        self.link = array( 'i', [ 0 ] )
+        self.gemTree.Branch( 'link', self.link, 'link/I' )
+        
+        self.Nev = array( 'i', [ 0 ] )
+        self.gemTree.Branch( 'Nev', self.Nev, 'Nev/I' )
+        
+        self.utime = array( 'i', [ 0 ] )
+        self.gemTree.Branch( 'utime', self.utime, 'utime/I' )
+        
+        self.ztrim = array( 'f', [ 0 ] )
+        self.gemTree.Branch( 'ztrim', self.ztrim, 'ztrim/F' )
+
+        self.vfatCH = array( 'i', [ 0 ] )
+        self.gemTree.Branch( 'vfatCH', self.vfatCH, 'vfatCH/I' )
+        
+        self.vfatID = array( 'i', [-1] )
+        self.gemTree.Branch( 'vfatID', self.vfatID, 'vfatID/I' ) #Hex Chip ID of VFAT
+
+        self.vfatN = array( 'i', [ -1 ] )
+        self.gemTree.Branch( 'vfatN', self.vfatN, 'vfatN/I' )
+
+        return
+
+    def autoSave(self, option="SaveSelf"):
+        self.gemTree.AutoSave(option)
+        return
+
+    def getMode(self):
+        return self.mode[0]
+    
+    def setDefaults(self, options, time):
+        """
+        Takes as input the options object returned by OptParser.parse_args()
+        see: https://docs.python.org/2/library/optparse.html
+
+        Sets values common to all scan scripts (see qcoptions.py)
+        """
+
+        self.link[0] = options.gtx
+        self.Nev[0] = options.nevts
+        self.utime[0] = time
+        self.ztrim[0] = options.ztrim
+
+        return
+
+    def write(self):
+        self.gemTree.Write()
+        return
+
+class gemDacCalTreeStructure(gemGenericTree):
+    def __init__(self, name, regX, valY, isGblDac=True, storeRoot=False, description="Generic GEM DAC Calibration Tree"):
+        """
+        name        TName of the TTree
+        regX        Register that is being calibrated (dependent variable)
+        valY        Value calibration is being performed against (e.g. scurveMean, or charge)
+        isGblDac    DAC is common across entire VFAT (True) or is specific for a given channel (False)
+        storeRoot   Store ROOT Objects Associated with this Dac Calibration
+        description Phrase describing the TTree
+        """
+        
+        gemGenericTree.__init__(name=name, description=description)
+
+        self.isGblDac = isGblDac
+        self.storeRoot = storeRoot
+
+        self.dacName = regX
+        self.gemTree.Branch( 'dacName', self.dacName, 'dacName/C')
+
+        self.dacValX = array( 'i', [0] )
+        self.gemTree.Branch( 'dacValX', self.dacValX, 'dacValX/I')
+
+        self.dacValX_Err = array( 'i', [0] )
+        self.gemTree.Branch( 'dacValX_Err', self.dacValX, 'dacValX_Err/I')
+
+        self.dacValY = array( 'i', [0] )
+        self.gemTree.Branch( 'dacValY', self.dacValY, 'dacValY/I')
+
+        self.dacValY_Err = array( 'i', [0] )
+        self.gemTree.Branch( 'dacValY_Err', self.dacValY, 'dacValY_Err/I')
+        
+        # Do not store channel level information for a global DAC
+        if self.isGblDac:
+            branch_vfatCH = self.gemTree.GetBranch('vfatCH')
+            self.gemTree.GetListOfBranches().Remove(branch_vfatCH)
+
+        if self.storeRoot:
+            self.g_dacCal = r.TGraphErrors()
+            self.gemTree.Branch( 'g_dacCal', self.g_dacCal)
+            
+            self.func_dacFit = r.TF1()
+            self.gemTree.Branch( 'func_dacFit', self.func_dacFit)
+
+        return
+
+    def fill(self, **kwargs):
+        """
+        Updates the values stored in the arrays gemTree's branchs map to,
+        then it fills the tree.
+
+        The keyword is assumed to the same as the variable name for 
+        simplicity.
+        """
+        
+        if "dacValX" in kwargs:
+            self.dacValX = kwargs["dacValX"]
+        if "dacValX_Err" in kwargs:
+            self.dacValX_Err = kwargs["dacValX_Err"]
+        if "dacValY" in kwargs:
+            self.dacValY = kwargs["dacValY"]
+        if "dacValY_Err" in kwargs:
+            self.dacValY_Err = kwargs["dacValY_Err"]
+        if "link" in kwargs:
+            self.link[0] = kwargs["link"]
+        if "Nev" in kwargs:
+            self.Nev[0] = kwargs["Nev"]
+        if "utime" in kwargs:
+            self.utime[0] = kwargs["utime"]
+        if (("vfatCH" in kwargs) and (not self.isGblDac)):
+            self.vfatCH[0] = kwargs["vfatCH"]
+        if "vfatID" in kwargs:
+            self.vfatID[0] = kwargs["vfatID"]
+        if "vfatN" in kwargs:
+            self.vfatN[0] = kwargs["vfatN"]
+        if "ztrim" in kwargs:
+            self.ztrim[0] = kwargs["ztrim"]
+
+        if self.storeRoot:
+            if "g_dacCal" in kwargs:
+                self.g_dacCal = kwargs["g_dacCal"].Clone()
+            if "func_dacFit" in kwargs:
+                self.func_dacFit = kwargs["func_dacFit"].Clone()
+
+        self.gemTree.Fill()
+        return
+
+class gemTreeStructure(gemGenericTree):
+    def __init__(self, name, description="Generic GEM TTree",scanmode=-1):
+        """
+        scanmode    scan type, e.g. scanmode.<TYPE> parameter
+        name        TName of the TTree
+        description Phrase describing the TTree
+        """
+        gemGenericTree.__init__(name=name, description=description,scanmode=scanmode)
 
         self.calPhase = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'calPhase', self.calPhase, 'calPhase/I' )
@@ -35,9 +179,6 @@ class gemTreeStructure:
         self.latency = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'latency', self.latency, 'latency/I' ) #used by ultraScurve, same physical quantity
         
-        self.link = array( 'i', [ 0 ] )
-        self.gemTree.Branch( 'link', self.link, 'link/I' )
-        
         self.pDel = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'pDel', self.pDel, 'pDel/I' )
         
@@ -47,9 +188,6 @@ class gemTreeStructure:
         
         self.mspl = array( 'i', [ -1 ] )
         self.gemTree.Branch( 'mspl', self.mspl, 'mspl/I' )
-        
-        self.Nev = array( 'i', [ 0 ] )
-        self.gemTree.Branch( 'Nev', self.Nev, 'Nev/I' )
         
         self.Nhits = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'Nhits', self.Nhits, 'Nhits/I' )
@@ -63,20 +201,8 @@ class gemTreeStructure:
         self.trimRange = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'trimRange', self.trimRange, 'trimRange/I' )
         
-        self.utime = array( 'i', [ 0 ] )
-        self.gemTree.Branch( 'utime', self.utime, 'utime/I' )
-        
         self.vcal = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'vcal', self.vcal, 'vcal/I' )
-        
-        self.vfatCH = array( 'i', [ 0 ] )
-        self.gemTree.Branch( 'vfatCH', self.vfatCH, 'vfatCH/I' )
-        
-        self.vfatID = array( 'i', [-1] )
-        self.gemTree.Branch( 'vfatID', self.vfatID, 'vfatID/I' ) #Hex Chip ID of VFAT
-
-        self.vfatN = array( 'i', [ -1 ] )
-        self.gemTree.Branch( 'vfatN', self.vfatN, 'vfatN/I' )
         
         self.vth = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'vth', self.vth, 'vth/I' )
@@ -90,13 +216,6 @@ class gemTreeStructure:
         self.vthr = array( 'i', [ 0 ] )
         self.gemTree.Branch( 'vthr', self.vthr, 'vthr/I' )
 
-        self.ztrim = array( 'f', [ 0 ] )
-        self.gemTree.Branch( 'ztrim', self.ztrim, 'ztrim/F' )
-
-        return
-
-    def autoSave(self, option="SaveSelf"):
-        self.gemTree.AutoSave(option)
         return
 
     def fill(self, **kwargs):
@@ -160,26 +279,4 @@ class gemTreeStructure:
             self.ztrim[0] = kwargs["ztrim"]
 
         self.gemTree.Fill()
-        return
-
-    def getMode(self):
-        return self.mode[0]
-    
-    def setDefaults(self, options, time):
-        """
-        Takes as input the options object returned by OptParser.parse_args()
-        see: https://docs.python.org/2/library/optparse.html
-
-        Sets values common to all scan scripts (see qcoptions.py)
-        """
-
-        self.link[0] = options.gtx
-        self.Nev[0] = options.nevts
-        self.utime[0] = time
-        self.ztrim[0] = options.ztrim
-
-        return
-
-    def write(self):
-        self.gemTree.Write()
         return
