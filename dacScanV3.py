63b4eeabf40044c695398fe1f271f5aab35521d8
Brian Dorney <brian.l.dorney@cern.ch>
2018-10-08 16:36:12 +0200

    bug fixes, and added debug table

--- dacScanV3.py
+++ dacScanV3.py
@@ -14,18 +14,21 @@ def getDACInfo():
     print("You've requested a VFAT3 DAC Scan")
     print("dac\tName")
     print("===\t====")
-    for dacVal,dacTuple in maxVfat3DACSize.iteritems():
-        print("%02d\t%s"%(dacVal,dacTuple[1]))
+    dacOptions = maxVfat3DACSize.keys()
+    dacOptions.sort()
+    for dacVal in dacOptions:
+        print("%02d\t%s"%(dacVal,maxVfat3DACSize[dacVal][1]))
     dacSelect=-1
     while(dacSelect not in maxVfat3DACSize.keys()):
-        dacSelect=raw_input("Please select the dac number from the options above: ")
-        if dacSelect not in maxVfat3DACSize.keys()):
+        dacSelect=int(raw_input("Please select the dac number from the options above: "))
+        if dacSelect not in maxVfat3DACSize.keys():
             print("Sorry input not understood") 
 
     retInfo = {
             "dacMax":maxVfat3DACSize[dacSelect][0],
             "dacMin":0,
-            "dacName":maxVfat3DACSize[dacSelect][1]
+            "dacName":maxVfat3DACSize[dacSelect][1],
+            "dacSelect":dacSelect
             }
 
     return retInfo
@@ -46,7 +49,8 @@ def scanAllLinks(args, calTree, vfatBoard):
     dictDACInfo = getDACInfo()
     dacMax = dictDACInfo["dacMax"]
     dacMin = dictDACInfo["dacMin"]
-    dacName = dictDACInfo["dacName"]
+    calTree.nameX[0] = dictDACInfo["dacName"]
+    dacSelect = dictDACInfo["dacSelect"]
 
     # Determine all Chip ID's
     from gempython.utils.nesteddict import nesteddict as ndict
@@ -54,35 +58,54 @@ def scanAllLinks(args, calTree, vfatBoard):
     print("Getting CHIP IDs of all VFATs")
     vfatIDvals = ndict()
     for ohN in range(0,12):
+        # Skip masked OH's
         if( not ((args.ohMask >> ohN) & 0x1)):
             continue
+
+        # update the OH in question
+        vfatBoard.parentOH.link = ohN
+
+        # Get the chip ID's
         vfatIDvals[ohN] = vfatBoard.getAllChipIDs(ohVFATMaskArray[ohN])
 
     # Perform DAC Scan
-    arraySize = bin(args.ohMask).count("1") * (dacMax-dacMin+1)*24/args.dacStep
+    arraySize = bin(args.ohMask).count("1") * (dacMax-dacMin+1)*24/args.stepSize
     scanData = (c_uint32 * arraySize)()
     print("Performing DAC Scan on all links, this may take some time please be patient")
-    rpcResp = amcBoard.performDacScanMultiLink(scanData,args.dacSelect,args.dacStep,args.ohMask,args.extRefADC)
+    rpcResp = amcBoard.performDacScanMultiLink(scanData,dacSelect,args.stepSize,args.ohMask,args.extRefADC)
     if rpcResp != 0:
         raise Exception('RPC response was non-zero, this inidcates an RPC exception occurred')
 
-    try:
-        for dacWord in scanData:
-            vfat = (dacWord >>18) & 0x1f
-            calTree.fill(
-                    dacValX = (dacWord & 0xff),
-                    dacValY = ((dacWord >> 8) & 0x3ff),
-                    dacValY_Err = 1, # convert to physical units in analysis, LSB is the error on Y
-                    link = ((dacWord >> 23) & 0xf),
-                    vfatID = vfatIDvals[vfat],
-                    vfatN = vfat
-                    )
-            pass
-    except Exception as error:
-        print("Exception raised: {}".format(error))
-        calTree.autoSave("SaveSelf")
-    #finally:
-    #    calTree.write()
+    #try:
+    if args.debug:
+        print("| link | vfatN | vfatID | nameX | dacValX | dacValX_Err | nameY | dacValY | dacValY_Err |")
+        print("| :--: | :---: | :----: | :-----: | :-----: | :---------: | :--: | :-----: | :---------: |")
+    for dacWord in scanData:
+        vfat = (dacWord >>18) & 0x1f
+        ohN = ((dacWord >> 23) & 0xf)
+        calTree.fill(
+                dacValX = (dacWord & 0xff),
+                dacValY = ((dacWord >> 8) & 0x3ff),
+                dacValY_Err = 1, # convert to physical units in analysis, LSB is the error on Y
+                link = ohN,
+                vfatID = vfatIDvals[ohN][vfat],
+                vfatN = vfat
+                )
+        if args.debug:
+            print("| {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} |".format(
+                calTree.link[0],
+                calTree.vfatN[0],
+                str(hex(calTree.vfatID[0])).strip('L'),
+                calTree.nameX[0],
+                calTree.dacValX[0],
+                calTree.dacValX_Err[0],
+                calTree.nameY[0],
+                calTree.dacValY[0],
+                calTree.dacValY_Err[0]))
+        pass
+    #except Exception as error:
+    #    print("Exception raised: {}".format(error))
+    #    calTree.autoSave("SaveSelf")
 
     print("DAC scans for optohybrids in {0} completed".format(args.ohMask))
 
@@ -101,43 +124,58 @@ def scanSingleLink(args, calTree, vfatBoard):
     dictDACInfo = getDACInfo()
     dacMax = dictDACInfo["dacMax"]
     dacMin = dictDACInfo["dacMin"]
-    dacName = dictDACInfo["dacName"]
+    calTree.nameX[0] = dictDACInfo["dacName"]
+    dacSelect = dictDACInfo["dacSelect"]
     
     # Determine VFAT mask
     if args.vfatmask is None:
         args.vfatmask = vfatBoard.parentOH.getVFATMask()
         if args.debug:
-            print("Automatically determined vfatmask to be: {0}".format(args.vfatmask))
+            print("Automatically determined vfatmask to be: {0}".format(str(hex(args.vfatmask)).strip('L')))
     
     # Determine Chip ID
     print("Getting CHIP IDs of all VFATs")
     vfatIDvals = vfatBoard.getAllChipIDs(args.vfatmask)
     
     # Perform DAC Scan
-    arraySize = (dacMax-dacMin+1)*24/args.dacStep
+    arraySize = (dacMax-dacMin+1)*24/args.stepSize
     scanData = (c_uint32 * arraySize)()
     print("Performing DAC Scan on Optohybrid {0}, this may take some time please be patient".format(vfatBoard.parentOH.link))
-    rpcResp = vfatBoard.parentOH.performDacScan(scanData, dacSelect, args.dacStep, args.vfatmask, args.extRefADC)
+    rpcResp = vfatBoard.parentOH.performDacScan(scanData, dacSelect, args.stepSize, args.vfatmask, args.extRefADC)
     if rpcResp != 0:
         raise Exception('RPC response was non-zero, this inidcates an RPC exception occurred')
 
     # Store Data
-    calTree.link = vfatBoard.parentOH.link
+    calTree.link[0] = vfatBoard.parentOH.link
 
-    try:
-        for dacWord in scanData:
-            vfat = (dacWord >>18) & 0x1f
-            calTree.fill(
-                    dacValX = (dacWord & 0xff),
-                    dacValY = ((dacWord >> 8) & 0x3ff),
-                    dacValY_Err = 1, # convert to physical units in analysis, LSB is the error on Y
-                    vfatID = vfatIDvals[vfat],
-                    vfatN = vfat
-                    )
-            pass
-    except Exception as error:
-        print("Exception raised: {}".format(error))
-        calTree.autoSave("SaveSelf")
+    #try:
+    if args.debug:
+        print("| link | vfatN | vfatID | nameX | dacValX | dacValX_Err | nameY | dacValY | dacValY_Err |")
+        print("| :--: | :---: | :----: | :-----: | :-----: | :---------: | :--: | :-----: | :---------: |")
+    for dacWord in scanData:
+        vfat = (dacWord >>18) & 0x1f
+        calTree.fill(
+                dacValX = (dacWord & 0xff),
+                dacValY = ((dacWord >> 8) & 0x3ff),
+                dacValY_Err = 1, # convert to physical units in analysis, LSB is the error on Y
+                vfatID = vfatIDvals[vfat],
+                vfatN = vfat
+                )
+        if args.debug:
+            print("| {0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} |".format(
+                calTree.link[0],
+                calTree.vfatN[0],
+                str(hex(calTree.vfatID[0])).strip('L'),
+                calTree.nameX[0],
+                calTree.dacValX[0],
+                calTree.dacValX_Err[0],
+                calTree.nameY[0],
+                calTree.dacValY[0],
+                calTree.dacValY_Err[0]))
+        pass
+    #except Exception as error:
+    #    print("Exception raised: {}".format(error))
+    #    calTree.autoSave("SaveSelf")
     #finally:
     #    calTree.write()
 
@@ -152,10 +190,11 @@ if __name__ == '__main__':
     """
 
     # create the parser
-    from argparse
+    import argparse
     parser = argparse.ArgumentParser(description="Scans a given DAC on a VFAT3 against the chip's ADC.  Either the internally or externally referenced ADC can be used.  Scans all VFATs on a given link simultaneously")
 
     # Positional arguments
+    from reg_utils.reg_interface.common.reg_xml_parser import parseInt
     parser.add_argument("cardName", type=str, help="hostname of the AMC you are connecting too, e.g. 'eagle64'; if running on an AMC use 'local' instead", metavar="cardName")
     parser.add_argument("ohMask", type=parseInt, help="ohMask to apply, a 1 in the n^th bit indicates the n^th OH should be considered", metavar="ohMask")
     
@@ -163,7 +202,7 @@ if __name__ == '__main__':
     from reg_utils.reg_interface.common.reg_xml_parser import parseInt
     parser.add_argument("-d","--debug", action="store_true", dest="debug",
             help = "Print additional debugging information")
-    paser.add_argument("-e","--extRefADC", action="store_true", dest="extRefADC",
+    parser.add_argument("-e","--extRefADC", action="store_true", dest="extRefADC",
             help = "Use the externally referenced ADC on the VFAT3.")
     parser.add_argument("-f","--filename",type=str,dest="filename",default="dacScanV3.root",
             help = "Specify output filename to store data in.")
@@ -198,32 +237,29 @@ if __name__ == '__main__':
     outF = r.TFile(args.filename,"RECREATE")
     calTree = gemDacCalTreeStructure(
                     name="dacScanTree",
-                    regX=dacName,
-                    regY=("ADC1" if args.extRefADC else "ADC0"),
-                    description="GEM DAC Calibration of DAC: {0}".format(dacName)
+                    nameX="dummy", # temporary name, will be over-ridden
+                    nameY=("ADC1" if args.extRefADC else "ADC0"),
+                    description="GEM DAC Calibration of VFAT3 DAC"
             )
 
-    try:
-        if args.series:
-            for ohN in range(0, amcBoard.nOHs+1):
-                if( not ((args.ohMask >> ohN) & 0x1)):
-                    continue
+    if args.series:
+        for ohN in range(0, amcBoard.nOHs+1):
+            if( not ((args.ohMask >> ohN) & 0x1)):
+                continue
 
-                # update the OH in question
-                vfatBoard.parentOH.link = ohN
+            # update the OH in question
+            vfatBoard.parentOH.link = ohN
 
-                scanSingleLink(args, calTree, vfatBoard)
-                pass
+            scanSingleLink(args, calTree, vfatBoard)
             pass
-        else:
-            scanAllLinks(args, calTree, vfatBoard)
-            pass
-    except Exception as error:
-        print("Exception Raised: {}".format(error))
-    finally:
-        calTree.autoSave("SaveSelf")
-        outF.cd()
-        calTree.write()
-        outF.Close()
+        pass
+    else:
+        scanAllLinks(args, calTree, vfatBoard)
+        pass
+
+    outF.cd()
+    calTree.autoSave("SaveSelf")
+    calTree.write()
+    outF.Close()
 
     print("All DAC Scans Completed. Goodbye")
