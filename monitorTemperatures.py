38a32a68a34c67e2cd0aee45a37ed9a603208f1e
Brian Dorney <brian.l.dorney@cern.ch>
2018-11-14 11:32:14 +0100

    cleaned up unnecessary comments

--- monitorTemperatures.py
+++ monitorTemperatures.py
@@ -44,7 +44,6 @@ if __name__ == '__main__':
         gemTempDataOH = gemTemepratureOHTree()
         gemTempDataVFAT = gemTemepratureVFATTree()
 
-    #from gempython.tools.amc_user_functions_xhal import *
     from gempython.tools.vfat_user_functions_xhal import *
     vfatBoard = HwVFAT(args.cardName, 0, args.debug) # Set a dummy link for now
     amcBoard = vfatBoard.parentOH.parentAMC
@@ -74,18 +73,8 @@ if __name__ == '__main__':
     amcBoard.configureVFAT3DacMonitorMulti(tempSelect, args.ohMask)
     print("VFAT ADCs have been configured for temperature monitoring")
 
-    # Remove this once a register for monitoring FPGA Core Temp Exists
-    #print("Configuring SCA JTAG Registers")
-    origSCAMonOffVal = amcBoard.readRegister("GEM_AMC.SLOW_CONTROL.SCA.ADC_MONITORING.MONITORING_OFF")
-    #from reg_utils.reg_interface.common.jtag import disableJtag, enableJtag, initJtagRegAddrs, jtagCommand
-    #initJtagRegAddrs()
-    ##enableJtag(args.ohMask,2)
-    ##enableJtag(args.ohMask,5)
-    ##enableJtag(args.ohMask,10)
-    #enableJtag(args.ohMask,20)
-
-    # This must come after enableJtag() otherwise Monitoring will be disabled
     print("Enabling SCA Monitoring")
+    origSCAMonOffVal = amcBoard.readRegister("GEM_AMC.SLOW_CONTROL.SCA.ADC_MONITORING.MONITORING_OFF")
     amcBoard.scaMonitorToggle(~args.ohMask & 0xFF)
     print("SCA Monitoring Enabled, set to {0}".format(str(hex(~args.ohMask & 0xFF)).strip('L')))
 
@@ -94,11 +83,8 @@ if __name__ == '__main__':
     adcDataIntRefMultiLinks = (c_uint32 * (24 * 12))()
     adcDataExtRefMultiLinks = (c_uint32 * (24 * 12))()
     try:
-        # place holder
         print("Reading and recording temperature data, press Ctrl+C to stop")
 
-        #from reg_utils.reg_interface.common.sca_utils import getOHlist
-        #from reg_utils.reg_interface.common.virtex6 import Virtex6Instructions
         while(True):
             # Read all ADCs
             print("Reading Internally Referenced ADC data")
@@ -117,11 +103,6 @@ if __name__ == '__main__':
             scaMonData = amcBoard.scaMonitorMultiLink(ohMask=args.ohMask)
 
             print("Reading FPGA Core Temperature")
-            #ohList = getOHlist(args.ohMask)
-            #jtagCommand(True, Virtex6Instructions.SYSMON, 10, 0x04000000, 32, False)
-            #adc1 = jtagCommand(False, None, 0, 0x04010000, 32, ohList)
-            ##adc1 = jtagCommand(True, None, 0, 0x04010000, 32, ohList)
-            #jtagCommand(True, Virtex6Instructions.BYPASS, 10, None, 0, False)
             sysmonData = amcBoard.sysmonMonitorMultiLink(ohMask=args.ohMask)
             print("Succeeded in reading FPGA Core Temperature")
 
@@ -158,10 +139,7 @@ if __name__ == '__main__':
                         continue
                     row = "| {0} | {1} | {2} |".format(
                             ohN,
-                            #scaMonData[ohN].ohFPGACoreTemp,
-                            #((adc1[ohN] >> 6) & 0x3FF) * 503.975 / 1024.0-273.15, # Remove once dedicated register exists
                             ((sysmonData[ohN].fpgaCoreTemp * 503.975) / 1024.0 - 273.15),
-                            #str(hex(sysmonData[ohN].fpgaCoreTemp)).strip('L'),
                             scaMonData[ohN].scaTemp)
                     for boardTemp in range(1,10):
                         row += " {0} |".format(scaMonData[ohN].ohBoardTemp[boardTemp-1])
@@ -202,8 +180,6 @@ if __name__ == '__main__':
                             ohBoardTemp7 = scaMonData[ohN].ohBoardTemp[6],
                             ohBoardTemp8 = scaMonData[ohN].ohBoardTemp[7],
                             ohBoardTemp9 = scaMonData[ohN].ohBoardTemp[8],
-                            #fpgaCoreTemp = scaMonData[ohN].ohFPGACoreTemp,
-                            #fpgaCoreTemp = (((adc1[ohN] >> 6) & 0x3FF) * 503.975 / 1024.0-273.15), # Remove once dedicated register exists
                             fpgaCoreTemp = ((sysmonData[ohN].fpgaCoreTemp * 503.975) / 1024.0 - 273.15),
                             link = ohN,
                             scaTemp = scaMonData[ohN].scaTemp,
